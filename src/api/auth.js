4973aa8d7416b8ecf457960a1fcf3fe144ece06a
sangkyeol <sangkyeol.kim@gmail.com>
2021-08-24 13:34:57 +0900

    api in progress

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,2 +1,17 @@
 import request from "./request";
 
+export const loginApi = (memberId, password) => {
+  return request("oauth/token", "post", null, null, {
+    memberId: memberId,
+    password: password,
+  });
+};
+export const joinApi = (memberId, password, name, phone, email) => {
+  return request("profile", "post", null, null, {
+    memberId: memberId,
+    password: password,
+    memberName: name,
+    telephoneNo: phone,
+    email: email,
+  });
+};
