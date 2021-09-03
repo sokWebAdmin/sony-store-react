a4e835fe7fe40535d8b03f924414dfda2b65c8cf
bomee.yoon <nanuya0331@gmail.com>
2021-09-03 22:01:06 +0900

    feat: add basic selectbox

---
+++ src/components/common/SelectBox.js
@@ -0,0 +1,45 @@
+import { React } from "react";
+
+/**
+ * @params selectOption = {
+ *            optionNo: number;
+ *            label: string;
+ *            disabled?: boolean;
+ *            useColor?: boolean;
+ *            background?: string(색상코드);
+ *          }[];
+ * @params selectOption (optionNo: number, label: string) => {};
+ */
+
+// @TODO color bomee.yoon
+export default function SelectBox({ selectOptions, selectOption }) {
+  
+  const emitSelectedOption = (event, optionNo, label) => {
+    event.preventDefault();
+    selectOption(optionNo, label);
+  };
+
+  return (
+    <ul class="select_opt">
+      {
+        selectOptions.map(option => {
+          const { label, optionNo } = option;
+          return (
+            <li>
+              <a href={label} className={ `opt_list ${ option?.disabled && 'disabled' }` } onClick={event => emitSelectedOption(event, optionNo)}>
+                <div class="item">
+                  if (option?.useColor) {
+                    <span class="circle_color">
+                    <span class="c_bg" style={{ background:option?.background }}></span>
+                    </span>
+                  }
+                  <span class="opt_name">{ label }</span>
+                </div>
+              </a>
+            </li>
+          )
+        })
+      }
+    </ul>
+  )
+}
