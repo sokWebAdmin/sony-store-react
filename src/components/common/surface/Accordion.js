d3e91cb845706f5bee258d8b957c8807d3aed739
andrea <kjkandrea@gmail.com>
2021-09-06 18:03:17 +0900

    Use Accordion

--- src/components/common/surface/Accordion.js
+++ src/components/common/surface/Accordion.js
@@ -1,7 +1,14 @@
 import '../../../assets/scss/interaction/accordion.scss'
 import { useRef } from 'react';
 
+/**
+ * @param prop
+ * title: string
+ * children: HTMLElements
+ */
 const Accordion = prop => {
+  const { title, children } = prop;
+
   const trigger = useRef(null)
   const content = useRef(null)
 
@@ -25,73 +32,14 @@ const Accordion = prop => {
   return (
     <div className="acc_item" ref={trigger}>
       <div className="acc_head">
-        <a className="acc_btn trigger" title="주문자 정보 열기" onClick={toggle}>
-          <span className="acc_tit">주문자 정보</span>
+        <a className="acc_btn trigger" title={title + ' 열기'} onClick={toggle}>
+          <span className="acc_tit">{title}</span>
           <span className="acc_arrow">상세 보기</span>
         </a>
       </div>
       <div className={`acc_inner parentNode`}>
         <div ref={content} className="acc_box">
-          <p className="acc_dsc_top">표시는 필수입력 정보</p>
-          <div className="acc_form">
-            <div className="acc_cell vat">
-              <label htmlFor="user_name">이름<i
-                className="necessary" /></label>
-            </div>
-            <div className="acc_cell">
-              <div
-                className="acc_group parent error">{/* error 문구 제어 */}
-                <div className="acc_inp type3">
-                  <input type="text" className="inp"
-                         id="user_name"
-                         placeholder="이름을 입력하세요."
-                         defaultValue="김소니" />
-                  <span className="focus_bg" />
-                </div>
-                <p className="error_txt"><span
-                  className="ico" />이름을 입력해 주세요.</p>
-              </div>
-            </div>
-          </div>
-          <div className="acc_form">
-            <div className="acc_cell vat">
-              <label htmlFor="user_email">이메일<i
-                className="necessary" /></label>
-            </div>
-            <div className="acc_cell">
-              <div
-                className="acc_group parent error">{/* error 문구 제어 */}
-                <div className="acc_inp type3">
-                  <input type="text" className="inp"
-                         id="user_email"
-                         placeholder="이메일 아이디 (예 : sony@sony.co.kr)"
-                         defaultValue="sony@sony.co.kr" />
-                  <span className="focus_bg" />
-                </div>
-                <p className="error_txt"><span
-                  className="ico" />이메일 아이디를 입력해 주세요.</p>
-              </div>
-            </div>
-          </div>
-          <div className="acc_form">
-            <div className="acc_cell vat">
-              <label htmlFor="user_number">휴대폰 번호<i
-                className="necessary" /></label>
-            </div>
-            <div className="acc_cell">
-              <div
-                className="acc_group parent error">{/* error 문구 제어 */}
-                <div className="acc_inp type3">
-                  <input type="text" className="inp"
-                         id="user_number"
-                         defaultValue={'01099999999'} />
-                  <span className="focus_bg" />
-                </div>
-                <p className="error_txt"><span
-                  className="ico" />휴대폰 번호를 입력해 주세요.</p>
-              </div>
-            </div>
-          </div>
+          {children}
         </div>
       </div>
     </div>
