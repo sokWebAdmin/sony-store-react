58d92539c025a3bbbd3bf42b7cc04d3d0eaaf103
andrea <kjkandrea@gmail.com>
2021-09-06 17:54:10 +0900

    Add evt

--- src/components/common/surface/Accordion.js
+++ src/components/common/surface/Accordion.js
@@ -1,14 +1,37 @@
+import '../../../assets/scss/interaction/accordion.scss'
+import { useRef } from 'react';
+
 const Accordion = prop => {
+  const trigger = useRef(null)
+  const content = useRef(null)
+
+  const toggle = () => {
+    const triggerNode = trigger.current
+    const contentNode = content.current
+    const contentParentNode = contentNode.parentNode;
+    const height = contentNode.clientHeight;
+    const isVisible = contentParentNode.clientHeight !== 0;
+
+    if (isVisible) {
+      contentParentNode.style.height = 0
+      triggerNode.classList.remove('on')
+      return;
+    }
+
+    contentParentNode.style.height = `${height}px`
+    triggerNode.classList.add('on')
+  }
+
   return (
-    <div className="acc_item on">
+    <div className="acc_item" ref={trigger}>
       <div className="acc_head">
-        <a className="acc_btn" title="주문자 정보 열기">
+        <a className="acc_btn trigger" title="주문자 정보 열기" onClick={toggle}>
           <span className="acc_tit">주문자 정보</span>
           <span className="acc_arrow">상세 보기</span>
         </a>
       </div>
-      <div className="acc_inner">
-        <div className="acc_box">
+      <div className={`acc_inner parentNode`}>
+        <div ref={content} className="acc_box">
           <p className="acc_dsc_top">표시는 필수입력 정보</p>
           <div className="acc_form">
             <div className="acc_cell vat">
