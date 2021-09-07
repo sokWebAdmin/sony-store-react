1a09e17ce9619a336fd7cd86f1d021b1e8be82f8
andrea <kjkandrea@gmail.com>
2021-09-07 10:38:12 +0900

    bind change evt

--- src/components/order/OrdererForm.js
+++ src/components/order/OrdererForm.js
@@ -12,7 +12,6 @@ const OrdererForm = prop => {
     }))
   }
 
-
   return (
     <>
       <div className="acc_form">
@@ -50,6 +49,9 @@ const OrdererForm = prop => {
               <input type="text" className="inp"
                      id="user_email"
                      placeholder="이메일 아이디 (예 : sony@sony.co.kr)"
+                     value={orderer.ordererContact1}
+                     name="ordererContact1"
+                     onChange={handleChange}
               />
               <span className="focus_bg" />
             </div>
@@ -69,6 +71,9 @@ const OrdererForm = prop => {
             <div className="acc_inp type3">
               <input type="text" className="inp"
                      id="user_number"
+                     value={orderer.ordererEmail}
+                     name="ordererEmail"
+                     onChange={handleChange}
               />
               <span className="focus_bg" />
             </div>
