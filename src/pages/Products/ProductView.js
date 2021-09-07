677ebcb1f862e27c1820bf1e0f2b6defea119136
BoMeeYoon <89110544+nanuya@users.noreply.github.com>
2021-09-07 14:04:30 +0900

    feat: add relatedProducts (#56)

--- src/pages/Products/ProductView.js
+++ src/pages/Products/ProductView.js
@@ -14,7 +14,7 @@ import 'swiper/components/scrollbar/scrollbar.scss';
 import "swiper/swiper.scss"
 
 //api
-import { getProductDetail, getProductOptions } from "../../api/product";
+import { getProductDetail, getProductOptions, getProductSearch } from "../../api/product";
 
 //css
 import "../../assets/scss/contents.scss"
@@ -24,20 +24,22 @@ import "../../assets/scss/product.scss"
 import GlobalContext from '../../context/global.context';
 
 //util
-import {useWindowSize} from '../../utils/utils'
-import { useHistory } from "react-router-dom";
+import {useWindowSize} from '../../utils/utils';
 import { getInfoLinks, getMainSliderStyle, mapContents } from './utils';
 import MainImage from './productView/MainImage';
 import TobContent from './productView/TopContent';
 import RelatedProducts from './productView/RelatedProducts';
 import Event from './productView/Event';
 import BottomContent from './productView/BottomContent';
+import { useHistory } from 'react-router';
+import qs from 'qs';
 
 //image
 
 
-export default function ProductView({ match }) {
+export default function ProductView({ match, ...props }) {
   const { productNo } = match.params;
+  const history = useHistory();
 
   //ui
   const [headerHeight, setHeaderHeight] = useState(0);
@@ -55,6 +57,7 @@ export default function ProductView({ match }) {
   const [productData, setProductData] = useState();
   const [productOptions, setProductOptions] = useState();
   const [contents, setContents] = useState([]);
+  const [relatedProducts, setRelatedProducts] = useState([]);
 
   // product init data
 
@@ -72,7 +75,21 @@ export default function ProductView({ match }) {
     mapProductData(ret.map(({ data }) => data));
   }, [mapProductData]);
 
+  const fetchRelatedProducts = useCallback(async (categories) => {
+    if (!categories) return;
+    
+    const ret = await getProductSearch({
+      'order.by': 'POPULAR',
+      categoryNos: categories
+                      .flatMap(({ categories }) => categories)
+                      .flatMap(({ categoryNo }) => categoryNo)
+                      .join()
+    });
+    setRelatedProducts(ret.data.items);
+  }, [])
+
   useEffect(() => fetchProductData(productNo), [fetchProductData, productNo]);
+  useEffect(() => fetchRelatedProducts(productData?.categories), [fetchRelatedProducts, productData?.categories])
 
   //
   const showProductDetail = useMemo(() => (headerHeight > 0 || size.height < 1280) && productData, [headerHeight, size.height, productData] )
@@ -96,7 +113,9 @@ export default function ProductView({ match }) {
                 productNo={productNo}
               />
             </div>
-            <RelatedProducts />
+            <RelatedProducts 
+              products={relatedProducts}
+            />
             <Event />
             <div className="product_cont full">
               <div className="relation_link">
