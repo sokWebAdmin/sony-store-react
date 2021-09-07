b8360c7a63c5b5c3b1092fbf51a37460d389308e
kimjongkeun <kjkandrea@gmail.com>
2021-09-07 15:50:49 +0900

    Add stylesheet

--- src/components/popup/FindAddress.js
+++ src/components/popup/FindAddress.js
@@ -3,6 +3,9 @@ import { useState } from 'react'
 // components
 import LayerPopup from "../common/LayerPopup"
 
+// stylesheet
+import '../../assets/scss/partials/popup/findAddress.scss'
+
 // 주소 찾기 팝업
 const FindAddress = () => {
   // LayerPopup 상태관리
@@ -21,11 +24,16 @@ const FindAddress = () => {
 
   return (
     <>
-      {test&& <LayerPopup className="login_chk_order" onClose={close}>
+      {test&& <LayerPopup className="find_address" onClose={close}>
         <>
-          <p className="pop_tit">로그인 후 주문해주세요.</p>
-          <p className="pop_txt">소니스토어 회원으로 로그인 하시고 다양한 멤버십 혜택을 누리세요!
-            비회원으로 제품을 구매하시면 소니스토어의 쿠폰 및 마일리지 적립 혜택을 받으실 수 없습니다. </p>
+          <p className="pop_tit">우편번호 찾기</p>
+          <form>
+            <input
+              type="text"
+              placeholder="검색어(도로명,지번,건물명)를 입력해주세요"
+            />
+            <button type="submit">검색</button>
+          </form>
           <div className="btn_article">
             <button className="button button_negative button-m closed" type="button" onClick={nonMemberOrder}>비회원 구매</button>
             <button className="button button_positive button-m" type="button" onClick={memberOrder}>회원 구매</button>
