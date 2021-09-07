ec9107ec58c0859e8f71104e4950cd8f0fa826a0
kimjongkeun <kjkandrea@gmail.com>
2021-09-07 15:58:26 +0900

    Add tip content

--- src/components/popup/FindAddress.js
+++ src/components/popup/FindAddress.js
@@ -35,6 +35,39 @@ const FindAddress = () => {
             />
             <button type="submit" className="search_button button button_negative">검색</button>
           </form>
+
+          <div id="tip" className="post-search-tip">
+            <h2>
+              TIP
+            </h2>
+            <p>
+              아래와 같이 검색하면 더욱 정확한 결과가 검색됩니다.
+            </p>
+            <dl>
+              <dt>
+                도로명 + 건물번호
+              </dt>
+              <dd>
+                예) 영동대로 513, 제주 첨단로 242
+              </dd>
+            </dl>
+            <dl>
+              <dt>
+                지역명(동/리) + 번지
+              </dt>
+              <dd>
+                예) 삼성동 25, 제주 영평동 2181
+              </dd>
+            </dl>
+            <dl>
+              <dt>
+                지역명(동/리) + 건물명(아파트명)
+              </dt>
+              <dd>
+                예) 분당 주공, 삼성동 코엑스
+              </dd>
+            </dl>
+          </div>
         </>
       </LayerPopup>}
       {/*   컴포넌트 내부 마크업   */}
