6d8bbcc8c0cf3e5a502fa1f9de5cd385b580e3d2
kimjongkeun <kjkandrea@gmail.com>
2021-09-07 16:27:52 +0900

    Change state name

--- src/components/popup/FindAddress.js
+++ src/components/popup/FindAddress.js
@@ -1,30 +1,32 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 // components
-import LayerPopup from "../common/LayerPopup"
+import LayerPopup from '../common/LayerPopup';
 
 // stylesheet
-import '../../assets/scss/partials/popup/findAddress.scss'
+import '../../assets/scss/partials/popup/findAddress.scss';
 
 // 주소 찾기 팝업
 const FindAddress = () => {
   // LayerPopup 상태관리
-  const [test, setTest] = useState(true)
+  const [visible, setVisible] = useState(true);
+
   const close = () => {
-    setTest(false);
-  }
+    setVisible(false);
+  };
+
   const nonMemberOrder = () => {
     console.log('비회원 구매');
     close();
-  }
+  };
   const memberOrder = () => {
     console.log('회원 구매');
     close();
-  }
+  };
 
   return (
     <>
-      {test&& <LayerPopup className="find_address" onClose={close}>
+      {visible && <LayerPopup className="find_address" onClose={close}>
         <>
           <p className="pop_tit">우편번호 찾기</p>
           <form className="search_container">
@@ -33,7 +35,9 @@ const FindAddress = () => {
               placeholder="도로명,지번,건물명 입력"
               className="search_input"
             />
-            <button type="submit" className="search_button button button_negative">검색</button>
+            <button type="submit"
+                    className="search_button button button_negative">검색
+            </button>
           </form>
 
           <div className="tip">
