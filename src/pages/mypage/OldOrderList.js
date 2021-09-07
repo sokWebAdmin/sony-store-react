93b9ffc923687b91be1f8d01b33820cc20d18fb5
KwonYG <andamirocll@gmail.com>
2021-09-07 23:04:52 +0900

    chore: add comments

--- src/pages/mypage/OldOrderList.js
+++ src/pages/mypage/OldOrderList.js
@@ -10,7 +10,8 @@ import '../../assets/scss/contents.scss';
 import '../../assets/scss/mypage.scss';
 
 export default function OldOrderList() {
-  //TODO: 스타일 전부 하나씩 scss 파일로 변환 하나설마
+  //TODO: 스타일 전부 하나씩 scss 파일로 변환 해야하나?>
+  // scss import 구문 주석 처리해도 괜찮던데,
   return (
     <>
       <SEOHelmet title={'구매상담 이용약관 동의'} />
