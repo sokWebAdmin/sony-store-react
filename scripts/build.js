e7fd0c627965624ab90c393364573f8d699dbd2e
dj.son <dj.son@godo.co.kr>
2021-09-13 14:34:36 +0900

    Fix react build option

--- scripts/build.js
+++ scripts/build.js
@@ -4,6 +4,13 @@
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
 
+const args = process.argv.slice(2);
+if (args[0] === 'development') {
+  process.env.BABEL_ENV = 'development';
+  process.env.NODE_ENV = 'development';
+}
+
+
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
@@ -48,7 +55,7 @@ const argv = process.argv.slice(2);
 const writeStatsJson = argv.indexOf('--stats') !== -1;
 
 // Generate configuration
-const config = configFactory('production');
+const config = configFactory(process.env.NODE_ENV);
 
 // We require that you explicitly set browsers and do not fall back to
 // browserslist defaults.
@@ -134,7 +141,7 @@ checkBrowsers(paths.appPath, isInteractive)
 
 // Create the production build and print the deployment instructions.
 function build(previousFileSizes) {
-  console.log('Creating an optimized production build...');
+  console.log(`Creating an optimized ${process.env.NODE_ENV} build...`);
 
   const compiler = webpack(config);
   return new Promise((resolve, reject) => {
