ff45ef20f8d83762f2de4c00f536dd07a4b916cd
KwonYG <andamirocll@gmail.com>
2021-09-13 09:28:33 +0900

    feat: add tab

---
+++ src/components/order/DateBox.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+
+export default function DateBox({ search }) {
+  const [selectMenu, setSelectMenu] = useState('threeM');
+
+  const onClickTab = (menu) => {
+    console.log('클릭:', menu);
+    setSelectMenu(menu);
+    search();
+  };
+
+  return (
+    <div class="date_box">
+      <ul class="date3_tab">
+        <li class={`tabs ${selectMenu === 'threeM' && 'on'}`}>
+          <a href="#" class="date3_btn" onClick={() => onClickTab('threeM')}>
+            3개월
+          </a>
+        </li>
+        <li class={`tabs ${selectMenu === 'sixM' && 'on'}`}>
+          <a href="#" class="date3_btn" onClick={() => onClickTab('sixM')}>
+            6개월
+          </a>
+        </li>
+        <li class={`tabs ${selectMenu === 'oneY' && 'on'}`}>
+          <a href="#" class="date3_btn" onClick={() => onClickTab('oneY')}>
+            1년
+          </a>
+        </li>
+      </ul>
+      <div class="date_rang">
+        <div class="calendar_box">
+          <input type="text" id="datepicker1" class="inp datepicker" autocomplete="off" />
+        </div>
+        <div class="calendar_box">
+          <input type="text" id="datepicker2" class="inp datepicker" autocomplete="off" />
+        </div>
+        <button class="button button_positive button-s" type="button">
+          조회
+        </button>
+      </div>
+    </div>
+  );
+}
