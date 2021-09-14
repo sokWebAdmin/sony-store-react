778671652c402d71983c20606ba0343a1c2a5aaa
kimjongkeun <kjkandrea@gmail.com>
2021-09-14 18:24:13 +0900

    Add data render & bind evt

--- src/components/popup/PickRecentAddresses.js
+++ src/components/popup/PickRecentAddresses.js
@@ -1,53 +1,55 @@
+import { useState, useEffect } from 'react';
+
 import LayerPopup from '../common/LayerPopup';
 
 const PickRecentAddresses = ({ recentAddresses }) => {
-  console.log(recentAddresses);
+
+  const [pickAddressNo, setPickAddressNo] = useState(null);
+
+  useEffect(() => {
+    if (!recentAddresses.length) {
+      return;
+    }
+
+    setPickAddressNo(recentAddresses[0].addressNo);
+  }, [recentAddresses]);
+
   return (
     <LayerPopup
       size={'m'}
-      popContClassName={'scrollH'}
+      popContClassName={'shipping_addr'}
     >
       <p className="pop_tit">최근 배송지</p>
       <p className="pop_txt">최근에 입력하신 배송지 5개까지 노출됩니다.</p>
       <div className="chk_select_zone">
         <ul className="chk_select_inner">
-          <li className="chk_select_list">
-            <div
-              className="chk_select_item label_click">
-              <div className="radio_box radio_only chk_select">
-                <input type="radio" className="inp_radio" id="addr_chk1"
-                       name="addr_chk1" checked="checked" />
-                <label htmlFor="addr_chk1"
-                       className="contentType">radio1</label>
-              </div>
-              <div className="chk_select_info">
-                <p className="txt">
-                  <strong className="name">김소니</strong>
-                  <span className="number">010 0000 0000</span>
-                </p>
-                <p className="txt">서울특별시 영등포구 여의도동 국제금융로 10 One IFC 24층
-                  ㈜소니코리아 (080080)</p>
-              </div>
-            </div>
-          </li>
-          <li className="chk_select_list">
-            <div
-              className="chk_select_item label_click">
-              <div className="radio_box radio_only chk_select">
-                <input type="radio" className="inp_radio" id="addr_chk2"
-                       name="addr_chk1" />
-                <label htmlFor="addr_chk2"
-                       className="contentType">radio1</label>
-              </div>
-              <div className="chk_select_info">
-                <p className="txt">
-                  <strong className="name">김소니</strong>
-                  <span className="number">010 0000 0000</span>
-                </p>
-                <p className="txt">서울시 구로구 구로동 12-1</p>
-              </div>
-            </div>
-          </li>
+          {
+            recentAddresses.map(address => (
+              <li className="chk_select_list" key={address.addressNo}
+                  onClick={() => setPickAddressNo(address.addressNo)}>
+                <div
+                  className="chk_select_item label_click">
+                  <div className="radio_box radio_only chk_select">
+                    <input type="radio" className="inp_radio"
+                           id={address.addressNo}
+                           name="addr_chk1"
+                           checked={pickAddressNo === address.addressNo}
+                           onChange={() => setPickAddressNo(
+                             address.addressNo)} />
+                    <label htmlFor={address.addressNo}
+                           className="contentType"></label>
+                  </div>
+                  <div className="chk_select_info">
+                    <p className="txt">
+                      <strong className="name">{address.receiverName}</strong>
+                      <span className="number">{address.receiverContact1}</span>
+                    </p>
+                    <p className="txt">{address.receiverAddress}</p>
+                  </div>
+                </div>
+              </li>
+            ))
+          }
         </ul>
       </div>
       <div className="btn_article">
