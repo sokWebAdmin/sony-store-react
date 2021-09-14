88505443430389f06974143caa99e2a2e7f104d1
KwonYG <andamirocll@gmail.com>
2021-09-14 17:11:20 +0900

    fix: add preventDefault to period tab

--- src/components/order/DateBox.js
+++ src/components/order/DateBox.js
@@ -8,7 +8,8 @@ export default function DateBox({ search }) {
   const [selectMenu, setSelectMenu] = useState('threeM');
   const [period, setPeriod] = useState({ startDate: new Date(addMonth(new Date(), -3)), endDate: new Date() });
 
-  const onClickTab = (menu) => {
+  const onClickTab = (e, menu) => {
+    e.preventDefault();
     const setPeriodDate = {
       threeM: () => setPeriod({ ...period, startDate: new Date(addMonth(new Date(), -3)) }),
       sixM: () => setPeriod({ ...period, startDate: new Date(addMonth(new Date(), -6)) }),
@@ -36,17 +37,17 @@ export default function DateBox({ search }) {
     <div class="date_box">
       <ul class="date3_tab">
         <li class={`tabs ${selectMenu === 'threeM' && 'on'}`}>
-          <a href="#" class="date3_btn" onClick={() => onClickTab('threeM')}>
+          <a href="#" class="date3_btn" onClick={(e) => onClickTab(e, 'threeM')}>
             3개월
           </a>
         </li>
         <li class={`tabs ${selectMenu === 'sixM' && 'on'}`}>
-          <a href="#" class="date3_btn" onClick={() => onClickTab('sixM')}>
+          <a href="#" class="date3_btn" onClick={(e) => onClickTab(e, 'sixM')}>
             6개월
           </a>
         </li>
         <li class={`tabs ${selectMenu === 'oneY' && 'on'}`}>
-          <a href="#" class="date3_btn" onClick={() => onClickTab('oneY')}>
+          <a href="#" class="date3_btn" onClick={(e) => onClickTab(e, 'oneY')}>
             1년
           </a>
         </li>
