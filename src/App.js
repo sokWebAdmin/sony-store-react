9bbbeffb1ed5cf80ca833c4580941dfe3edc1587
choisohyun <sohyun4283@naver.com>
2021-09-15 16:11:45 +0900

    Fix event router

--- src/App.js
+++ src/App.js
@@ -213,11 +213,11 @@ const App = (props) => {
 
               {/* 이벤트  */}
               <Route exact path="/event/list" component={eventList} />
+              <Route exact path="/event/only/:eventNo" component={EventDetail} />
               <Route exact path="/event/detail/:eventNo" component={EventDetail} />
-              <Route exact path="/event/only" component={Only} />
               <Route exact path="/event/expired" component={Expired} />
-              <Route exact path="/event/benefit-zone" component={BenefitZone} />
-              <Route exact path="/event/pre-order" component={PreOrder} />
+              <Route exact path="/event/benefit-zone" component={BenefitZone} />{/*현재 데이터 없음*/}
+              <Route exact path="/event/pre-order" component={PreOrder} />{/*현재 데이터 없음*/}
               {/*<Route exact path="/event/asc" component={asc} />*/}
               <Route exact path="/event/refined" component={refined} />
               <Route exact path="/event/live-on" component={liveon} />
