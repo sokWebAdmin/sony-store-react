fcb08d7703aa921a5af7861be4e5d0c3b5e8fe44
KwonYG <andamirocll@gmail.com>
2021-09-15 16:51:40 +0900

    feat: add profile order cancel api

--- src/api/claim.js
+++ src/api/claim.js
@@ -7,3 +7,6 @@ export const putProfileClaimRefundAccountByClaimNo = ({
   path: { claimNo },
   requestBody: { bank, account, depositorName },
 }) => request(`profile/claims/${claimNo}/account`, 'put', null, { bank, account, depositorName });
+
+export const postProfileClaimOrderCancelByOrderNo = ({ path: { orderNo }, requestBody }) =>
+  request(`profile/orders/${orderNo}/claims/cancel`, 'post', null, requestBody);
