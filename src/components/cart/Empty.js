b070941401bf79826af2aa4383253a7f22d61d99
kimjongkeun <kjkandrea@gmail.com>
2021-09-16 15:50:49 +0900

    Add go home link

--- src/components/cart/Empty.js
+++ src/components/cart/Empty.js
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const Empty = () => {
   return (
     <div className="empty_cart_box">
       <i className="empty_ico"></i>
       <p className="emptyinfo_tit">장바구니에 담긴 상품이 없습니다.</p>
       <div className="btn_box">
-        <button type="button" className="button button_negative">쇼핑 계속 하기
-        </button>
+        <Link to="/" type="button" className="button button_negative">쇼핑 계속 하기
+        </Link>
       </div>
     </div>
   );
