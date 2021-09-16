3e214bf17174421af62a1edb60cef675f14fd407
kimjongkeun <kjkandrea@gmail.com>
2021-09-16 16:25:18 +0900

    Add ls util

---
+++ src/utils/storage.js
@@ -0,0 +1,59 @@
+const { localStorage } = window;
+
+const EPD_KEY = '_expirationDate';
+
+export const setWithExpire = (
+  name,
+  data,
+  expire = 60 * 60 * 24 * 2, // default: 2Day
+) => {
+  const unix = Date.now() + expire;
+
+  set(name, data);
+  set(name + EPD_KEY, unix);
+};
+
+export const getWithExpire = name => {
+  const unix = this.getItem(name + EPD_KEY);
+  const expire = Boolean(unix) && unix < Date.now();
+
+  return expire ? null : get(name);
+};
+
+export const remove = (name) => {
+  try {
+    localStorage.removeItem(name);
+    return true;
+  }
+  catch (err) {
+    console.error(
+      'removeStorage: Error removing key [' + name + '] from localStorage: ' +
+      JSON.stringify(err));
+  }
+  return false;
+};
+
+function get (name) {
+  try {
+    return JSON.parse(localStorage.getItem(name));
+  }
+  catch (err) {
+    console.error(
+      'getStorage: Error reading key [' + name + '] from localStorage: ' +
+      JSON.stringify(err));
+  }
+  return null;
+};
+
+function set (name, value) {
+  try {
+    localStorage.setItem(name, JSON.stringify(value));
+    return true;
+  }
+  catch (err) {
+    console.error(
+      'setStorage: Error setting key [' + name + '] in localStorage: ' +
+      JSON.stringify(err));
+  }
+  return false;
+};
