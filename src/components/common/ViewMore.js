2475f877a45c704a7ea251a8cbf2d8ad9560a978
bomee.yoon <nanuya0331@gmail.com>
2021-09-17 08:12:21 +0900

    fix: viewmore error

--- src/components/common/ViewMore.js
+++ src/components/common/ViewMore.js
@@ -1,25 +1,19 @@
-import { useEffect, useMemo, useRef } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { debounce } from 'lodash';
 
 export default function ViewMore({ totalCount, viewMore, pageSize, reset = false }) {
-  const pageNumber = useRef(1);
+  const [ pageNumber, setPageNumber ] = useState(1);
   const hide = useMemo(() => (pageNumber * pageSize) > totalCount, [ pageNumber, pageSize, totalCount ])
 
   const debounceViewMore = debounce((pageNumber) => viewMore(pageNumber), 500);
 
   const handleCountClick = event => {
     event.preventDefault();
-
-    pageNumber.current += 1;
     debounceViewMore(pageNumber + 1);
-    // setPageNumber(prev => prev += 1);
+    setPageNumber(prev => prev += 1);
   };
 
-  useEffect(() => {
-    if (reset) {
-      pageNumber.current = 1;
-    }
-  }, [reset])
+  useEffect(() => reset && setPageNumber(1), [reset])
 
   return (
     <div className="btn_article comm_more" style={{ visibility: hide ? 'hidden' : 'visible' }}>
