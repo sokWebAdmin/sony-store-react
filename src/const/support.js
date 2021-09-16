d7c146eb89776d62fff2ea3aecbf20c9c66f80c0
bomee.yoon <nanuya0331@gmail.com>
2021-09-16 19:05:17 +0900

    refactor: storeInfo.js

---
+++ src/const/support.js
@@ -0,0 +1,28 @@
+// 직영점
+export const MAP_CLIENT_ID = process.env.REACT_APP_MAP_CLIENT_ID || 'i1nv00h8zo';
+export const COORDINATE = {
+  lat: 37.52369,
+  lng: 127.03901
+};
+export const storeImages = [
+  {
+    src:'../../images/event/img_store1.jpg',
+    alt: '소니스토어 압구정점 내부 이미지 1'
+  },
+  {
+    src:'../../images/event/img_store2.jpg',
+    alt: '소니스토어 압구정점 내부 이미지 2'
+  },
+  {
+    src:'../../images/event/img_store3.jpg',
+    alt: '소니스토어 압구정점 내부 이미지 3'
+  },
+  {
+    src:'../../images/event/img_store4.jpg',
+    alt: '소니스토어 압구정점 내부 이미지 4'
+  },
+  {
+    src:'../../images/event/img_store5.jpg',
+    alt: '소니스토어 압구정점 내부 이미지 5'
+  },
+];
