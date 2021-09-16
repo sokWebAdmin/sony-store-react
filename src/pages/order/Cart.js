79e7f949ba132d088bb9ace9bb64b5790470eacd
kimjongkeun <kjkandrea@gmail.com>
2021-09-16 16:06:07 +0900

    Add empty cart check : member

--- src/pages/order/Cart.js
+++ src/pages/order/Cart.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { useHistory } from 'react-router';
 
 // components
@@ -12,9 +12,35 @@ import ProductList from '../../components/cart/ProductList';
 //css
 import '../../assets/scss/contents.scss';
 import '../../assets/scss/order.scss';
+import GlobalContext from '../../context/global.context';
+import { getCart } from '../../api/order';
 
 const Cart = () => {
   const history = useHistory();
+  const { isLogin } = useContext(GlobalContext);
+
+  const [cartEmpty, setCartEmpty] = useState(false);
+
+  const init = () => {
+    if (isLogin) {
+      fetchCart();
+    }
+  };
+
+  useEffect(init, []);
+
+  async function fetchCart () {
+    try {
+      const { data } = await getCart();
+      if (data.deliveryGroups.length < 1) {
+        setCartEmpty(true);
+        return;
+      }
+    }
+    catch (err) {
+      console.error(err);
+    }
+  }
 
   return (
     <>
@@ -24,20 +50,23 @@ const Cart = () => {
           <div className="content order_page">
             <div className="order_box">
               <Header />
-
-              <Empty />
-
-              <ProductList />
-              <div className="button_wrap">
-                <a className="button button_negative">쇼핑 계속 하기</a>
-                <button type="submit"
-                        className="button button_positive popup_comm_btn"
-                        data-popup-name="login_chk_order" onClick={() => {
-                  history.push('/order/sheet');
-                }}>구매하기
-                </button>
-              </div>
-              <QnA />
+              {cartEmpty
+                ? <Empty />
+                :
+                <>
+                  <ProductList />
+                  <div className="button_wrap">
+                    <a className="button button_negative">쇼핑 계속 하기</a>
+                    <button type="submit"
+                            className="button button_positive popup_comm_btn"
+                            data-popup-name="login_chk_order" onClick={() => {
+                      history.push('/order/sheet');
+                    }}>구매하기
+                    </button>
+                  </div>
+                  <QnA />
+                </>
+              }
             </div>
           </div>
         </div>
