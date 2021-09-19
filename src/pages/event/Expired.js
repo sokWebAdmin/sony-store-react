fa0b47e6f335dc1e78f0041cb8c52faf2e6cdb15
choisohyun <sohyun4283@naver.com>
2021-09-20 00:25:12 +0900

    Add temp expired events

--- src/pages/event/Expired.js
+++ src/pages/event/Expired.js
@@ -1,43 +1,103 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import "../../assets/scss/event.scss"
+import { getDisplayEvents } from '../../api/display';
+import { getStrDate } from '../../utils/dateFormat';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
+import SelectBox from '../../components/common/SelectBox';
+
+const tags = {
+  all: '',
+  only: '소니스토어단독',
+  'benefit-zone': '혜택존',
+  'pre-order': '예약판매',
+  refined: '정품등록이벤트',
+  'live-on': 'LIVEON',
+  employee: '임직원몰',
+  refurbish: '리퍼비시몰',
+};
 
 const Expired = () => {
+  const [events, setEvents] = useState([]);
+  const [newest, setNewest] = useState(true);
+  const [keyword, setKeyword] = useState('');
+
+  const fetchInitDisplayEvents = async (keyword = '') => {
+    const { data } = await getDisplayEvents(keyword);
+    sortEvents(data);
+  }
+
+  const sortEvents = (data = events) => {
+    const sortByLatestCreationDate = (a, b) => {
+      const dateL = moment(a.startYmdt)
+        .toDate()
+        .getTime();
+      const dateR = moment(b.startYmdt)
+        .toDate()
+        .getTime();
+      return dateL < dateR ? 1 : -1;
+    };
+    const sortByOldestCreationDate = (a, b) => {
+      const dateL = moment(a.startYmdt)
+        .toDate()
+        .getTime();
+      const dateR = moment(b.startYmdt)
+        .toDate()
+        .getTime();
+      return dateL > dateR ? 1 : -1;
+    };
+    const sortData = newest ? [...data].sort(sortByLatestCreationDate) : [...data].sort(sortByOldestCreationDate);
+    setEvents(sortData);
+  }
+
+  const getLink = (origin = true, eventNo, tagName, event) => {
+    const key = Object.keys(tags).find(key => tagName.includes(tags[key]));
+
+    if (event.url !== '') {
+      return `${origin ? document.location.origin : ''}/${event.url}/${eventNo}`;
+    }
+    if (key === 'all') {
+      return `${origin ? document.location.origin : ''}/event/detail/${eventNo}`;
+    } else if (key === 'only') {
+      return `${origin ? document.location.origin : ''}/event/${key}/${eventNo}`;
+    }
+    return `${origin ? document.location.origin : ''}/event/${key}`;
+  };
+
+  useEffect(() => {
+    fetchInitDisplayEvents();
+  }, []);
+
   return (
     <>
       <div className="container full">
         <div className="content">
           <div className="common_head first_tit">
-            <a href="../../html/evnet/eventList.html" className="common_head_back">기획전</a>
+            <Link to="/event/list" className="common_head_back">기획전</Link>
             <h1 className="common_head_name">종료된 기획전</h1>
           </div>
           <div className="end_event">
             <form action="">
               <div className="search_zone">
-                <div className="select_ui_zone btm_line search_category">
-                  <a href="#" className="selected_btn" data-default-text="제목">
-                    제목
-                  </a>
-                  <div className="select_inner">
-                    <p className="prd_tag">제목</p>
-                    <ul className="select_opt">
-                      <li>
-                        <a href="#" className="opt_list">
-                          <div className="item">제목</div>
-                        </a>
-                      </li>
-                      <li>
-                        <a href="#" className="opt_list">
-                          <div className="item">내용</div>
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div className="group search_box">
+                <SelectBox
+                  defaultInfo={{
+                    type: 'dropdown',
+                    placeholder: '태그',
+                    className: 'search_category'
+                  }}
+                  selectOptions={[
+                    {
+                      optionNo: '01',
+                      label: '태그'
+                    },
+                  ]}
+                  selectOption={() => console.log('')}
+                />
+                <div className="group search_box" onClick={() => fetchInitDisplayEvents(keyword)}>
                   <div className="inp_box">
                     <input type="text" id="dd" className="search_inp" placeholder="검색어를 입력하세요." autoComplete="off"
-                           maxLength="50" />
+                           maxLength="50" onChange={(e) => setKeyword(e.target.value)} value={keyword} />
                   </div>
                 </div>
               </div>
@@ -50,10 +110,14 @@ const Expired = () => {
                 </button>
                 <div className="itemsort__drawer">
                   <ul className="itemsort__items">
-                    <li className="itemsort__item itemsort__item--active"><a href="#"
-                                                                             className="itemsort__item__link">최신순</a>
+                    <li className={`itemsort__item ${newest ? 'itemsort__item--active' : ''}`}>
+                      <a href="javascript:void(0)"
+                         className="itemsort__item__link" onClick={() => setNewest(true)}>최신순</a>
                     </li>
-                    <li className="itemsort__item"><a href="#" className="itemsort__item__link">오래된 순</a></li>
+                    <li className={`itemsort__item ${!newest ? 'itemsort__item--active' : ''}`}>
+                      <a href="javascript:void(0)" className="itemsort__item__link" onClick={() => setNewest(false)}>오래된
+                        순</a>
+                    </li>
                   </ul>
                 </div>
               </div>
@@ -72,191 +136,29 @@ const Expired = () => {
                     </div>
                   </div>
                   <div className="col_table_body">
-                    <div className="col_table_row">
-                      <div className="col_table_cell event_num">
-                        <p className="txt">10</p>
-                      </div>
-                      <div className="col_table_cell divide">
-                        <div className="divide_table">
-                          <div className="table_cell tal event_name">
-                            <p className="txt">공간을 밝히는 스피커 SRS-XP500 런칭 이벤트</p>
+                    {events && events.map((event, index) => {
+                      const {eventNo, label, startYmdt, endYmdt, tag} = event;
+                      return (
+                        <div className="col_table_row" key={eventNo}>
+                          <div className="col_table_cell event_num">
+                            <p className="txt">{index + 1}</p>
                           </div>
-                          <div className="table_cell event_duration">
-                            <p className="txt">21.04.08 ~ 21.05.31</p>
+                          <div className="col_table_cell divide">
+                            <div className="divide_table">
+                              <div className="table_cell tal event_name">
+                                <Link to={getLink(false, eventNo, tag, event)} className="txt">{label}</Link>
+                              </div>
+                              <div className="table_cell event_duration">
+                                <p className="txt">{getStrDate(startYmdt, 'YY.MM.DD')} ~ {getStrDate(endYmdt, 'YY.MM.DD')}</p>
+                              </div>
+                            </div>
                           </div>
+                          {/*<div className="col_table_cell event_result">*/}
+                          {/*  <a href="#" className="button button_secondary button-s" type="button">당첨자 발표 확인</a>*/}
+                          {/*</div>*/}
                         </div>
-                      </div>
-                      <div className="col_table_cell event_result">
-                        <a href="#" className="button button_secondary button-s" type="button">당첨자 발표 확인</a>
-                      </div>
-                    </div>
-                    <div className="col_table_row">
-                      <div className="col_table_cell event_num">
-                        <p className="txt">9</p>
-                      </div>
-                      <div className="col_table_cell divide">
-                        <div className="divide_table">
-                          <div className="table_cell tal event_name">
-                            <p className="txt">산뜻한 일상의 시작 올인원</p>
-                          </div>
-                          <div className="table_cell event_duration">
-                            <p className="txt">21.04.08 ~ 21.05.31</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col_table_cell event_result">
-                        <p className="txt"><a href="#" className="button button_secondary button-s" type="button">당첨자 발표
-                          확인</a></p>
-                      </div>
-                    </div>
-                    <div className="col_table_row">
-                      <div className="col_table_cell event_num">
-                        <p className="txt">8</p>
-                      </div>
-                      <div className="col_table_cell divide">
-                        <div className="divide_table">
-                          <div className="table_cell tal event_name">
-                            <p className="txt">산뜻한 일상의 시작 올인원</p>
-                          </div>
-                          <div className="table_cell event_duration">
-                            <p className="txt">21.04.08 ~ 21.05.31</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col_table_cell event_result">
-                        <p className="txt"></p>
-                      </div>
-                    </div>
-                    <div className="col_table_row">
-                      <div className="col_table_cell event_num">
-                        <p className="txt">7</p>
-                      </div>
-                      <div className="col_table_cell divide">
-                        <div className="divide_table">
-                          <div className="table_cell tal event_name">
-                            <p className="txt">산뜻한 일상의 시작 올인원 스피커 CMT-X3CD</p>
-                          </div>
-                          <div className="table_cell event_duration">
-                            <p className="txt">21.04.08 ~ 21.05.31</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col_table_cell event_result">
-                        <p className="txt"></p>
-                      </div>
-                    </div>
-                    <div className="col_table_row">
-                      <div className="col_table_cell event_num">
-                        <p className="txt">6</p>
-                      </div>
-                      <div className="col_table_cell divide">
-                        <div className="divide_table">
-                          <div className="table_cell tal event_name">
-                            <p className="txt">산뜻한 일상의 시작 올인원 스피커 CMT-X3CD</p>
-                          </div>
-                          <div className="table_cell event_duration">
-                            <p className="txt">21.04.08 ~ 21.05.31</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col_table_cell event_result">
-                        <p className="txt"></p>
-                      </div>
-                    </div>
-                    <div className="col_table_row">
-                      <div className="col_table_cell event_num">
-                        <p className="txt">5</p>
-                      </div>
-                      <div className="col_table_cell divide">
-                        <div className="divide_table">
-                          <div className="table_cell tal event_name">
-                            <p className="txt">산뜻한 일상의 시작 올인원</p>
-                          </div>
-                          <div className="table_cell event_duration">
-                            <p className="txt">21.04.08 ~ 21.05.31</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col_table_cell event_result">
-                        <p className="txt"><a href="#" className="button button_secondary button-s" type="button">당첨자 발표
-                          확인</a></p>
-                      </div>
-                    </div>
-                    <div className="col_table_row">
-                      <div className="col_table_cell event_num">
-                        <p className="txt">4</p>
-                      </div>
-                      <div className="col_table_cell divide">
-                        <div className="divide_table">
-                          <div className="table_cell tal event_name">
-                            <p className="txt">산뜻한 일상의 시작 올인원 스피커 CMT-X3CD</p>
-                          </div>
-                          <div className="table_cell event_duration">
-                            <p className="txt">21.04.08 ~ 21.05.31</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col_table_cell event_result">
-                        <p className="txt"></p>
-                      </div>
-                    </div>
-                    <div className="col_table_row">
-                      <div className="col_table_cell event_num">
-                        <p className="txt">3</p>
-                      </div>
-                      <div className="col_table_cell divide">
-                        <div className="divide_table">
-                          <div className="table_cell tal event_name">
-                            <p className="txt">산뜻한 일상의 시작 올인원 스피커 CMT-X3CD</p>
-                          </div>
-                          <div className="table_cell event_duration">
-                            <p className="txt">21.04.08 ~ 21.05.31</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col_table_cell event_result">
-                        <p className="txt"><a href="#" className="button button_secondary button-s" type="button">당첨자 발표
-                          확인</a></p>
-                      </div>
-                    </div>
-                    <div className="col_table_row">
-                      <div className="col_table_cell event_num">
-                        <p className="txt">2</p>
-                      </div>
-                      <div className="col_table_cell divide">
-                        <div className="divide_table">
-                          <div className="table_cell tal event_name">
-                            <p className="txt">산뜻한 일상의 시작 올인원 스피커 CMT-X3CD</p>
-                          </div>
-                          <div className="table_cell event_duration">
-                            <p className="txt">21.04.08 ~ 21.05.31</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col_table_cell event_result">
-                        <p className="txt"><a href="#" className="button button_secondary button-s" type="button">당첨자 발표
-                          확인</a></p>
-                      </div>
-                    </div>
-                    <div className="col_table_row">
-                      <div className="col_table_cell event_num">
-                        <p className="txt">1</p>
-                      </div>
-                      <div className="col_table_cell divide">
-                        <div className="divide_table">
-                          <div className="table_cell tal event_name">
-                            <p className="txt">산뜻한 일상의 시작 올인원</p>
-                          </div>
-                          <div className="table_cell event_duration">
-                            <p className="txt">21.04.08 ~ 21.05.31</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col_table_cell event_result">
-                        <p className="txt"><a href="#" className="button button_secondary button-s" type="button">당첨자 발표
-                          확인</a></p>
-                      </div>
-                    </div>
+                      )
+                    })}
                   </div>
                 </div>
                 <div className="btn_article comm_more">
