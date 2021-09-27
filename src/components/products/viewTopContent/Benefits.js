4e66ff6d3d3af517bb79a3b825ff415ff5113a0d
bomee.yoon <nanuya0331@gmail.com>
2021-09-27 14:47:56 +0900

    feat: add benefits

--- src/components/products/viewTopContent/Benefits.js
+++ src/components/products/viewTopContent/Benefits.js
@@ -1,32 +1,35 @@
+import { useMallState } from "../../../context/mall.context";
+import { getPricePerProduct } from "../../../utils/product";
 import { wonComma } from "../../../utils/utils";
 
 // 할인 혜택
 export default function Benefits({ price }) {
+
+  const mallInfo = useMallState();
+
+  const { discount } = getPricePerProduct(price);
+
+  const unit = mallInfo?.accumulationUnit;
+  
   return (
     <div className="cont line">
       <p className="tit">회원별 마일리지 적립혜택 <span className="icon_question">!</span></p>
       <ul className="membership_rating">
-        <li className="vvip">{/* class 별 등급 색상 지정 vvip / vip / family */}
-          <span className="mark">VV</span>
-          <div className="save_info">
-            <span className="percentage">VVIP 8%</span>
-            <p className="mileage"><span className="num">{wonComma(price.salePrice * 0.08)}</span> P</p>
-          </div>
-        </li>
-        <li className="vip">
-          <span className="mark">V</span>
-          <div className="save_info">
-            <span className="percentage">VIP 4%</span>
-            <p className="mileage"><span className="num">{wonComma(price.salePrice * 0.04)}</span> P</p>
-          </div>
-        </li>
-        <li className="family">
-          <span className="mark">M</span>
-          <div className="save_info">
-            <span className="percentage">MEMBERSHIP 2%</span>
-            <p className="mileage"><span className="num">{wonComma(price.salePrice * 0.02)}</span> P</p>
-          </div>
-        </li>
+        {
+          mallInfo?.mall?.grades.filter(({ reserveBenefit }) => reserveBenefit.reserveRate > 0).map((g, idx) => {
+            const UPPERCASE = g.label.toUpperCase();
+            const LOWERCASE = g.label.toLowerCase();
+            return (
+              <li key={`${g.label}${idx}`} className={g.label === 'Membership' ? 'family' : LOWERCASE}>{/* class 별 등급 색상 지정 vvip / vip / family */}
+                <span className="mark">{UPPERCASE.includes('VIP') ? UPPERCASE.split('IP')[0] : UPPERCASE}</span>
+                <div className="save_info">
+                  <span className="percentage">{UPPERCASE} {g.reserveBenefit.reserveRate}%</span>
+                  <p className="mileage"><span className="num">{wonComma(discount * (g.reserveBenefit.reserveRate/100))}</span> {unit}</p>
+                </div>
+              </li>
+            )
+          })
+        }
       </ul>
     </div>
   )
