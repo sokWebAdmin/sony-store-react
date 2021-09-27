413da969e558c9d0c860b806bfe09ba2e2eb108d
kimjongkeun <kjkandrea@gmail.com>
2021-09-27 18:39:53 +0900

    Add add cart

--- src/components/myPage/main/WishList.js
+++ src/components/myPage/main/WishList.js
@@ -1,8 +1,13 @@
 import { useState, useMemo } from 'react';
 import { toCurrencyString } from '../../../utils/unit.js';
-import { postProfileLikeProducts } from '../../../api/product';
+import {
+  getProductsOptions,
+  postProfileLikeProducts,
+} from '../../../api/product';
 import Confirm from '../../common/Confirm';
 import Alert from '../../common/Alert';
+import { postCart } from '../../../api/order';
+import Notification from '../../products/Notification';
 
 const WishList = ({ wishList, wishCount, more, rerender }) => {
   const [checkedProductNos, setCheckedProductNos] = useState([]);
@@ -51,9 +56,27 @@ const WishList = ({ wishList, wishCount, more, rerender }) => {
       catch(console.error);
   };
 
+  const addToCart = async () => {
+    const { data: { optionInfos } } = await getProductsOptions(
+      { productNos: checkedProductNos });
+    const request = optionInfos.map(product => ({
+      productNo: product.mallProductNo,
+      optionNo: product.options[0].optionNo,
+      orderCnt: 1,
+    }));
+    const { status } = await postCart(request);
+    if (status === 200) {
+      setCartNotify(true);
+    }
+    else {
+      alert('장바구니 담기 실패');
+    }
+  };
+
   const [allDeleteConfirm, setAllDeleteConfirm] = useState(false);
   const [deleteConfirm, setDeleteConfirm] = useState(false);
   const [checkAlert, setCheckAlert] = useState(false);
+  const [cartNotify, setCartNotify] = useState(false);
 
   const handleAllDeleteConfirm = status => {
     if (status === 'ok') {
@@ -83,6 +106,8 @@ const WishList = ({ wishList, wishCount, more, rerender }) => {
       <Confirm onClose={handleDeleteConfirm}>선택한 제품을 삭제하시겠습니까?</Confirm>}
       {checkAlert &&
       <Alert onClose={() => setCheckAlert(false)}>제품을 선택해주세요.</Alert>}
+      {cartNotify &&
+      <Notification setNotificationVisible={setCartNotify} type='cart' />}
 
       <div className="cont_head">
         <h3 className="cont_tit" id="wish-tit">찜</h3>
@@ -94,7 +119,7 @@ const WishList = ({ wishList, wishCount, more, rerender }) => {
           </button>
           <button
             className="button button_positive button-s popup_comm_btn"
-            type="button" data-popup-name="cart_pop">
+            type="button" data-popup-name="cart_pop" onClick={addToCart}>
             <span>선택 제품</span> 장바구니 담기
           </button>
         </div>
