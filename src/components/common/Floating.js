8b22924e758a4f862e88159160d9171a46b4e184
BoMeeYoon <89110544+nanuya@users.noreply.github.com>
2021-09-30 07:31:00 +0900

    update floating (#293)

--- src/components/common/Floating.js
+++ src/components/common/Floating.js
@@ -1,4 +1,4 @@
-import { useEffect, useContext, useState } from "react";
+import { useEffect, useContext, useState, useMemo } from "react";
 import { useHistory } from "react-router";
 import GlobalContext from "../../context/global.context";
 import { useAlert, useScroll, useToggle } from "../../hooks";
@@ -8,9 +8,12 @@ import Confirm from "./Confirm";
 const SERVICE_CENTER = process.env.REACT_APP_KAKAO_SERVICE_CENTER_KEY; // http://pf.kakao.com/_xbxhExaj
 const JS_KEY = process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY; // @FIXME 소니코리아 고객센터의 jsKey 로 연결해야 함.
 
+const TEL = '1588-0911';
+const MOBILE_WIDTH = 640;
+
 const message = {
-  alert: '고객님께서 원하시는 제품을 \n 빠르고 정확하게 구매하실 수 있도록 도와드리겠습니다. \n 고객지원센터: 1588-0911',
-  confirm: '카톡 상담을 위해선 로그인이 필요합니다. \n 로그인 하시겠습니까?'
+  alert: `고객님께서 원하시는 제품을 <br /> 빠르고 정확하게 구매하실 수 있도록 <br />도와드리겠습니다. <br /> 고객지원센터: 1588-0911`,
+  confirm: `카톡 상담을 위해선 로그인이 필요합니다. <br /> 로그인 하시겠습니까?`
 };
 
 const chat = () => {
@@ -19,6 +22,9 @@ const chat = () => {
 
 export default function Floating () {
   const history = useHistory();
+  
+  const windowWidth = window.innerWidth;
+  const isMobile = useMemo(() => windowWidth <= MOBILE_WIDTH, [windowWidth]);
   
   const { isLogin } = useContext(GlobalContext);
   const [ active, toggle ] = useToggle(false);
@@ -44,6 +50,8 @@ export default function Floating () {
   }
 
   const handleClick = (e, type) => {
+    if (isMobile && type === 'cs') return;
+
     e.preventDefault();
     switch(type) {
       case 'kakao':
@@ -69,10 +77,10 @@ export default function Floating () {
     <nav className={`sidebar ${scrollY >= 300 && 'sidebar--visible'} ${active && 'sidebar--active'}`}>
       <div className="sidebar__inner">
         <a href="#none" onClick={ e => handleClick(e, 'kakao') } className="sidebar__btn sidebar__btn__link kakao"><span>카톡 상담</span></a>
-        <a href="#none" onClick={ e=> handleClick(e, 'cs') } className="sidebar__btn sidebar__btn__link customer"><span>고객 센터</span></a>
-        <a href="#header" onClick={ e=> handleClick(e, 'top') } className="sidebar__btn top"><span>페이지 상단</span></a>
+        <a href={isMobile ? `tel:${TEL}` : '#none'} onClick={ e=> handleClick(e, 'cs') } className="sidebar__btn sidebar__btn__link customer"><span>고객 센터</span></a>
+        <a href="#header" onClick={ e => handleClick(e, 'top') } className="sidebar__btn top"><span>페이지 상단</span></a>
       </div>
-      <button onClick={toggle} className="sidebar__btn sidebar__btn__toggle" type="button"><img src="../../images/common/ic_sidebar1.svg" alt="메뉴토글" /></button>
+      <button onClick={ () => active ? toggle(false) : toggle() } className="sidebar__btn sidebar__btn__toggle" type="button"><img src="../../images/common/ic_sidebar1.svg" alt="메뉴토글" /></button>
       { alertVisible && <Alert onClose={closeModal}>{alertMessage}</Alert> }
       { confirmVisible && <Confirm onClose={closeConfirm}>{message.confirm}</Confirm> }
     </nav>
