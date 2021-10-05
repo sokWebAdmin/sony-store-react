cd6646f7fcffa7651f82fa004ec57fad1d066012
kimjongkeun <kjkandrea@gmail.com>
2021-10-05 14:17:20 +0900

    Add agent/scheme test

--- src/utils/detectAgent.js
+++ src/utils/detectAgent.js
@@ -1,9 +1,33 @@
+// osType : '01' | '02'
+const osTypeReferee = osType => {
+  if (osType === '01') {
+    return 'android';
+  }
+  if (osType === '02') {
+    return 'ios';
+  }
+  return 'unknown';
+};
+
+/*
+ * return {
+ *   isApp: boolean,
+ *   device: 'android'|'ios'|'unknown'
+ * }
+ */
 export const getAgent = () => {
   const { userAgent } = window.navigator;
   const isWeb = !userAgent.includes('osType');
   if (isWeb) {
     return {
       isApp: false,
+    };
+  }
+  const osTypeIndex = userAgent.indexOf('osType/');
+  if (!osTypeIndex) {
+    return {
+      isApp: true,
+      device: osTypeReferee(userAgent.substr(osTypeIndex + 7, 2)),
     };
   }
 
