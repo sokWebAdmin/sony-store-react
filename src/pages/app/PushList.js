1b4bb90b4cbcc44aabc2f41e5cf0127cf7a4b6b4
kimjongkeun <kjkandrea@gmail.com>
2021-10-05 16:00:24 +0900

    Add push list page

---
+++ src/pages/app/PushList.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useContext } from 'react';
+
+//SEO
+import SEOHelmet from '../../components/SEOHelmet';
+
+const PushList = () => {
+  return (
+    <>
+      <SEOHelmet title={`PUSH 알림 메세지함`} />
+
+    </>
+  );
+};
