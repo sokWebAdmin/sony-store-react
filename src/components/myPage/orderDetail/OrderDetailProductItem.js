335766d7cfc15e88fa7589449775fb32b3260d33
KwonYG <andamirocll@gmail.com>
2021-10-05 16:00:00 +0900

    fix: remove option title on order detail

--- src/components/myPage/orderDetail/OrderDetailProductItem.js
+++ src/components/myPage/orderDetail/OrderDetailProductItem.js
@@ -11,7 +11,7 @@ export default function OrderDetailProductItem({ productName, imageUrl, optionTi
           </div>
           <div className="prd_info">
             <div className="prd_info_name">{productName}</div>
-            <p className="prd_info_option">{optionTitle}</p>
+            {/* <p className="prd_info_option">{optionTitle}</p> */}
           </div>
         </div>
       </div>
