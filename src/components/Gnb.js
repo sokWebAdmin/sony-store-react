a3d775a32382253627f0fc3953b011fb13a7b46e
kimjongkeun <kjkandrea@gmail.com>
2021-10-05 15:28:26 +0900

    Add agent info

--- src/components/Gnb.js
+++ src/components/Gnb.js
@@ -6,7 +6,7 @@ import { useCategoryState } from '../context/category.context';
 import { getAgent } from '../utils/detectAgent.js';
 
 export default function Gnb () {
-  const agent = JSON.stringify(getAgent()); // test code
+  const agent = getAgent(); // test code
 
   const history = useHistory();
   const headerDispatch = useHeaderDispatch();
@@ -90,7 +90,7 @@ export default function Gnb () {
           })}
         </ul>
       </nav>
-      <h1>app config : {agent}</h1>
+      <h1>app config : {JSON.stringify(agent)}</h1>
       {
         agent.isApp &&
         <a href="sonyapp://closemall/qr" style={{
