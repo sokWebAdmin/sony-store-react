3308882e86284944c492fa66c109ad14cebeadf3
choisohyun <sohyun4283@naver.com>
2021-10-06 10:56:11 +0900

    Fix expired event style

--- src/pages/event/Expired.js
+++ src/pages/event/Expired.js
@@ -8,39 +8,14 @@ import SelectBox from '../../components/common/SelectBox';
 import ViewMore from '../../components/common/ViewMore';
 import SEOHelmet from '../../components/SEOHelmet';
 
-const tags = {
-  all: '',
-  only: '소니스토어단독',
-  'benefit-zone': '혜택존',
-  'pre-order': '예약판매',
-  refined: '정품등록이벤트',
-  'live-on': 'LIVEON',
-  employee: '임직원몰',
-  refurbish: '리퍼비시몰',
-};
-
 const Expired = () => {
-  const [events, setEvents] = useState([]);
+  const [events, setEvents] = useState({items: [], totalCount: 0});
   const [keyword, setKeyword] = useState('');
 
   const fetchInitDisplayEvents = async (pageNumber = 1, keyword = '') => {
     const query = keyword ? { keyword: keyword.trim(), pageNumber } : { pageNumber };
     const { data } = await getDisplayCloseEvents({ ...query, hasTotalCount: true });
-    setEvents(data);
-  };
-
-  const getLink = (origin = true, eventNo, tagName, event) => {
-    const key = Object.keys(tags).find(key => tagName.includes(tags[key]));
-
-    if (event.url !== '') {
-      return `${origin ? document.location.origin : ''}/${event.url}/${eventNo}`;
-    }
-    if (key === 'all') {
-      return `${origin ? document.location.origin : ''}/event/detail/${eventNo}`;
-    } else if (key === 'only') {
-      return `${origin ? document.location.origin : ''}/event/${key}/${eventNo}`;
-    }
-    return `${origin ? document.location.origin : ''}/event/${key}`;
+    setEvents({totalCount: data.totalCount, items: events.items.concat(data.items)});
   };
 
   useEffect(() => {
@@ -50,7 +25,7 @@ const Expired = () => {
   return (
     <>
       <SEOHelmet title={'종료된 기획전'} />
-      <div className="container full">
+      <div className="container full expired">
         <div className="content">
           <div className="common_head first_tit">
             <Link to="/event/list" className="common_head_back">기획전</Link>
@@ -103,7 +78,7 @@ const Expired = () => {
                   </div>
                   <div className="col_table_body">
                     {events?.items && events.items.map((event, index) => {
-                      const { eventNo, label, startYmdt, endYmdt, tag } = event;
+                      const { eventNo, label, startYmdt, endYmdt } = event;
                       return (
                         <div className="col_table_row" key={eventNo}>
                           <div className="col_table_cell event_num">
