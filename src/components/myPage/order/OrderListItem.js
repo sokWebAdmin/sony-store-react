9c64823e2a1aee97c1e9da36c134b36aeed571aa
KwonYG <andamirocll@gmail.com>
2021-10-06 15:07:44 +0900

    chore: remove comments

--- src/components/myPage/order/OrderListItem.js
+++ src/components/myPage/order/OrderListItem.js
@@ -71,8 +71,7 @@ export default function OrderListItem({
   };
 
   const showRefundAccountInfo = (claimStatusType, payType) => {
-    // return claimStatusType === 'CANCEL_REQUEST' && payType === 'VIRTUAL_ACCOUNT';
-    return true;
+    return claimStatusType === 'CANCEL_REQUEST' && payType === 'VIRTUAL_ACCOUNT';
   };
 
   const onClickRefundAccount = () => setRefundAccountVisible(true);
