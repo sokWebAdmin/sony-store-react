d5564ea0b6d18e124b48befacbce52df77531916
choisohyun <sohyun4283@naver.com>
2021-10-07 15:58:33 +0900

    Add push list api

--- src/pages/app/PushList.js
+++ src/pages/app/PushList.js
@@ -1,4 +1,5 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import { useHistory } from 'react-router-dom';
 
 //SEO
 import SEOHelmet from '../../components/SEOHelmet';
@@ -6,15 +7,21 @@ import SEOHelmet from '../../components/SEOHelmet';
 // stylesheet
 import '../../assets/scss/app.scss';
 import { getPushs } from '../../api/sony/member';
+import { getStrDate } from '../../utils/dateFormat';
+import ViewMore from '../../components/common/ViewMore';
 
 const PushList = () => {
-  const test = async () => {
-    const data = await getPushs({formData: '14', rowsPerPage: 10, pageIdx: 1});
+  const history = useHistory();
+  const [pushData, setPushData] = useState({items: [], totalCount: 0});
+
+  const fetchPushList = async (pageIdx = 1) => {
+    const { data } = await getPushs({fromDate: '14', rowsPerPage: 10, pageIdx});
     console.log(data);
+    setPushData({items: data.body, totalCount: data.paginationInfo.totalCount});
   }
 
   useEffect(() => {
-    test();
+    fetchPushList();
   }, [])
 
   return (
@@ -23,7 +30,7 @@ const PushList = () => {
       <div className="wrapper">
         <div className="container" id="container">
           <div className="app_head">
-            <a href="#" className="back_btn">뒤로 가기</a>
+            <a href="/" className="back_btn">뒤로 가기</a>
             <h1 className="app_head_tit">PUSH 알림 메시지함</h1>
           </div>
           <div className="app_guide">
@@ -33,34 +40,27 @@ const PushList = () => {
               <li>운영정책에 따라 예고 없이 삭제 될 수 있습니다.</li>
             </ul>
           </div>
-          <div className="app_push_inner">
+          {pushData.totalCount <= 0 ? <div className="app_push_inner">
             <p className="ico_box_tit">
               <i className="ico_none_list"></i>
               <strong className="ico_tit">조회 내역이 존재하지 않습니다.</strong>
             </p>
           </div>
+            :
           <div className="app_push_inner">
             <ul className="app_push_list">
-              <li className="lists">
-                <span className="category">공지</span>
-                <strong className="tit">추석 연휴 소니스토어 압구정 휴무 안내</strong>
-                <span className="duration">21.04.08 ~ 21.05.31</span>
-              </li>
-              <li className="lists">
-                <span className="category">이벤트</span>
-                <strong className="tit">8월 14일은 뮤직데이 뮤직데이기념 SALE 위크</strong>
-                <span className="duration">21.04.08 ~ 21.05.31</span>
-              </li>
-              <li className="lists">
-                <span className="category">이벤트</span>
-                <strong className="tit">메가스터디와 함께하는 주간완전학습 플래너 증정 이벤트</strong>
-                <span className="duration">21.04.08 ~ 21.05.31</span>
-              </li>
+              {pushData.items.map((item) => {
+                return (
+                  <li className="lists" onClick={() => history.push(item.linkUrl)}>
+                    <span className="category">공지</span>
+                    <strong className="tit">{item.title}</strong>
+                    <span className="duration">{getStrDate(item.resvDate)} ~ ??</span>
+                  </li>
+                )
+              })}
             </ul>
-            <div className="btn_article comm_more line">
-              <a href="#" className="more_btn" title="기획전 더보기">더보기</a>
-            </div>
-          </div>
+            <ViewMore totalCount={pushData.totalCount} pageSize={10} viewMore={fetchPushList} />
+          </div>}
         </div>
       </div>
     </>
