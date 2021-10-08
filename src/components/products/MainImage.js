0be02ce82383b793a7b1c07e7a2cf9674cfb90c7
bomee.yoon <nanuya0331@gmail.com>
2021-10-08 21:43:32 +0900

    fix swiper navigation error

--- src/components/products/MainImage.js
+++ src/components/products/MainImage.js
@@ -6,46 +6,33 @@ export default function MainImage({ imageUrls }) {
   SwiperCore.use([Navigation, Pagination, Scrollbar, Autoplay, Controller]);
   const prevRef = useRef(null);
   const nextRef = useRef(null);
-  const hasImgs = imageUrls.length > 1;
+  const paginationRef = useRef(null);
   return (
     <div className="view_slider swiper-container">
       <Swiper
-        className="swiper-wrapper"
-        slidesPerView={ 1 }
-        pagination={{
-          el: '.swiper-pagination',
-          type: 'bullets',
-          clickable: true,
-        }}
-        observer={true}
-        navigation={{
-          prevEl: prevRef.current,
-          nextEl: nextRef.current,
-        }}
-        on={{
-              init: swiper => {
-                swiper.params.navigation.prevEl = prevRef.current;
-                swiper.params.navigation.nextEl = nextRef.current;
-                swiper.navigation.update();
-              },
-            }}
-        >
-        {
-          imageUrls?.map((image, index) => (
-            <SwiperSlide 
-              key={`${image}${index}`} 
-              className="swiper-slide"
-            >
-              <img src={image} alt="상품 이미지" />
-            </SwiperSlide>
-          ))
-        }
-        <div className="arrow_btn">
-              <button className="arrow swiper-button-prev banner-prev" ref={prevRef}><img src="/images/common/arrow_19_34.png" alt="이전" /></button>
-              <button className="arrow swiper-button-next banner-next" ref={nextRef}><img src="/images/common/arrow_19_34.png" alt="다음" /></button>
-            </div>
-        <div className="swiper-pagination" />
-      </Swiper>
+      onInit={(swiper) => {
+        swiper.params.pagination.el = paginationRef.current;
+        swiper.params.pagination.bullets = imageUrls.length;
+        swiper.params.pagination.clickable = true;
+        swiper.pagination.render();
+        swiper.pagination.update();
+
+        swiper.params.navigation.prevEl = prevRef.current;
+        swiper.params.navigation.nextEl = nextRef.current;
+        swiper.navigation.init();
+        swiper.navigation.update();
+      }}
+      loop={true}
+    >
+      {
+        imageUrls?.map((image, i) => <SwiperSlide className="swiper-slide" key={i}><img src={image} alt="상품이미지" /></SwiperSlide>)
+      }
+      <div className="arrow_btn">
+        <button className="product_prev product_arrow" ref={prevRef}><img src="/images/common/arrow_19_34.png" alt="이전" /></button>
+        <button className="product_next product_arrow" ref={nextRef}><img src="/images/common/arrow_19_34.png" alt="다음" /></button>
+      </div>
+      <div ref={paginationRef}></div>
+    </Swiper>
     </div>
   ) 
 }
