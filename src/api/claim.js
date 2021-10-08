67bc0a64ad45eee31596e952e62000a114c00cdd
dj.son <dj.son@godo.co.kr>
2021-10-08 17:22:56 +0900

    Fix 주문 취소시 환불계좌등록 팝업 노출 로직 수정 (#443

--- src/api/claim.js
+++ src/api/claim.js
@@ -5,6 +5,11 @@ export const putProfileClaimRefundAccountByClaimNo = ({
   requestBody: { bank, account, depositorName },
 }) => request(`profile/claims/${claimNo}/account`, 'put', null, { bank, account, depositorName });
 
+export const putGuestClaimRefundAccountByClaimNo = ({
+  path: { claimNo },
+  requestBody: { bank, account, depositorName },
+}) => request(`guest/claims/${claimNo}/account`, 'put', null, { bank, account, depositorName });
+
 export const postProfileClaimOrderCancelByOrderNo = ({ path: { orderNo }, requestBody }) =>
   request(`profile/orders/${orderNo}/claims/cancel`, 'post', null, requestBody);
 
