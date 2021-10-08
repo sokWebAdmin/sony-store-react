ac0338b42bcd881ffc162b8d2506818185f6b012
bomee.yoon <nanuya0331@gmail.com>
2021-10-08 21:52:57 +0900

    fix swiper pagination bug

--- src/components/products/MainImage.js
+++ src/components/products/MainImage.js
@@ -7,20 +7,27 @@ export default function MainImage({ imageUrls }) {
   const prevRef = useRef(null);
   const nextRef = useRef(null);
   const paginationRef = useRef(null);
+
+  const setPagination = swiper => {
+    swiper.params.pagination.el = paginationRef.current;
+    swiper.params.pagination.bullets = imageUrls.length;
+    swiper.params.pagination.clickable = true;
+    swiper.pagination.render();
+    swiper.pagination.update();
+  };
+
+  const setNavigation = swiper => {
+    swiper.params.navigation.prevEl = prevRef.current;
+    swiper.params.navigation.nextEl = nextRef.current;
+    swiper.navigation.init();
+    swiper.navigation.update();
+  }
   return (
     <div className="view_slider swiper-container">
       <Swiper
       onInit={(swiper) => {
-        swiper.params.pagination.el = paginationRef.current;
-        swiper.params.pagination.bullets = imageUrls.length;
-        swiper.params.pagination.clickable = true;
-        swiper.pagination.render();
-        swiper.pagination.update();
-
-        swiper.params.navigation.prevEl = prevRef.current;
-        swiper.params.navigation.nextEl = nextRef.current;
-        swiper.navigation.init();
-        swiper.navigation.update();
+        setPagination(swiper);
+        setNavigation(swiper);
       }}
       loop={true}
     >
@@ -31,7 +38,7 @@ export default function MainImage({ imageUrls }) {
         <button className="product_prev product_arrow" ref={prevRef}><img src="/images/common/arrow_19_34.png" alt="이전" /></button>
         <button className="product_next product_arrow" ref={nextRef}><img src="/images/common/arrow_19_34.png" alt="다음" /></button>
       </div>
-      <div ref={paginationRef}></div>
+      <div className="product_pagination" ref={paginationRef}></div>
     </Swiper>
     </div>
   ) 
