5df1578f56a28aa28b3589ca9048437d16375620
kimjongkeun <kjkandrea@gmail.com>
2021-10-08 11:20:59 +0900

    Add agent platform

--- src/components/order/orderPayment.js
+++ src/components/order/orderPayment.js
@@ -1,15 +1,25 @@
 import { isMobile } from 'react-device-detect';
 import { getAccessToken } from '../../utils/token';
+import { getAgent } from '../../utils/detectAgent';
+
+const agent = getAgent();
 
 const orderPayment = {
   get NCPPay () { // NCPPay context
     return window.NCPPay;
   },
+  get platform () {
+    if (!agent.isApp) {
+      return isMobile ? 'MOBILE_WEB' : 'PC';
+    }
+    // ios 아닌경우 android 로 판단. android 특정 기기에서 이슈있다면 이곳 확인할것
+    return agent.device === 'ios' ? 'IOS' : 'AOS';
+  },
   get config () {
     return {
       clientId: 'MzuMctQTZBXWmdTlujFy3Q==',
       accessToken: getAccessToken(),
-      platform: isMobile ? 'MOBILE_WEB' : 'PC',
+      platform: this.platform,
       confirmUrl: `${window.location.origin}/order/parse`,
     };
   },
@@ -17,7 +27,6 @@ const orderPayment = {
     this.NCPPay.setConfiguration(this.config);
   },
   post (requestBody) {
-    console.log(requestBody);
     this.NCPPay.reservation(requestBody);
   },
   run (requestBody) {
