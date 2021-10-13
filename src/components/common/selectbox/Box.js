536b6429de24ea81a83685bee28488b12cecf13e
kimeunbi <ebkim@godo.co.kr>
2021-10-13 11:20:44 +0900

    Fix selectBox style

--- src/components/common/selectbox/Box.js
+++ src/components/common/selectbox/Box.js
@@ -1,70 +1,75 @@
-import { useRef, useState } from "react";
+import { useRef, useState } from 'react';
 
-export default function BoxSelector({ selectOptions, onToggleHandler, onClickHandler, display, selectedLabel, tag, open }) {
+export default function BoxSelector({
+  selectOptions,
+  onToggleHandler,
+  onClickHandler,
+  display,
+  selectedLabel,
+  tag,
+  open,
+}) {
   const [bg, setBg] = useState(null);
   const selectRef = useRef(null);
   const setSelectClassName = () => {
     const currentClassList = selectRef.current.classList;
     const isOpened = currentClassList.contains('open');
     !isOpened ? currentClassList.add('open') : currentClassList.remove('open');
-  }
+  };
   return (
     <div ref={selectRef} className="select_ui_zone select_zone">
       <div>
         <a
           href="#select"
           className="selected_btn"
-          onClick={ event => {
+          onClick={(event) => {
             onToggleHandler(event);
             setSelectClassName();
-          } }
-        > 
-          {
-            bg && (
-                <span className="circle_color">
-                  <span className="c_bg" style={{ background:bg }}></span>
-                </span>
-              )
-          }
-          { selectedLabel }
+          }}
+        >
+          {bg && (
+            <span className="circle_color">
+              <span className="c_bg" style={{ background: bg }}></span>
+            </span>
+          )}
+          {selectedLabel}
         </a>
-        <div className="select_inner" style={{ display }}>
-          <p className="prd_tag">{ tag }</p>
+        <div className="select_inner" style={{ display, zIndex: 100 }}>
+          <p className="prd_tag">{tag}</p>
           <ul className="select_opt">
-            {
-              selectOptions.map(option => {
-                const { label, optionNo } = option;
-                return (
-                  <li key={ optionNo }>
-                    <a
-                      href={ `#${label}` }
-                      className={`opt_list ${option?.disabled && 'disabled'}`}
-                      onClick={ event => {
-                        onClickHandler(event, option);
-                        setBg(option?.background);
-                      }}
-                    >
-                      <div className="item">
-                        {
-                          option?.background && (
-                              <span className="circle_color">
-                                <span className="c_bg" style={{ background:option?.background }}></span>
-                              </span>
-                            )
-                        }
-                        <span className="opt_name">{ label }{ option?.disabledLabel && ` (${option.disabledLabel})` }</span>
-                      </div>
-                    </a>
-                  </li>
-                )
-              })
-            }
+            {selectOptions.map((option) => {
+              const { label, optionNo } = option;
+              return (
+                <li key={optionNo}>
+                  <a
+                    href={`#${label}`}
+                    className={`opt_list ${option?.disabled && 'disabled'}`}
+                    onClick={(event) => {
+                      onClickHandler(event, option);
+                      setBg(option?.background);
+                    }}
+                  >
+                    <div className="item">
+                      {option?.background && (
+                        <span className="circle_color">
+                          <span className="c_bg" style={{ background: option?.background }}></span>
+                        </span>
+                      )}
+                      <span className="opt_name">
+                        {label}
+                        {option?.disabledLabel && ` (${option.disabledLabel})`}
+                      </span>
+                    </div>
+                  </a>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </div>
     </div>
   );
-};
+}
 
 BoxSelector.defaultProps = {
   defaultInfo: {
@@ -72,4 +77,4 @@ BoxSelector.defaultProps = {
     placeholder: '제품을 선택해주세요.',
     tag: '제품',
   },
-}
+};
