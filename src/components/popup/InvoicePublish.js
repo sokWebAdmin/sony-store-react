07221229f57af00983525de2b5bce4ee4c51a6b9
kimeunbi <ebkim@godo.co.kr>
2021-10-15 18:15:58 +0900

    Fix: InvoicePublish popup height

--- src/components/popup/InvoicePublish.js
+++ src/components/popup/InvoicePublish.js
@@ -111,7 +111,7 @@ const InvoicePublish = ({ isView, basketid, close }) => {
   return (
     <LayerPopup size={'m'} onClose={close} popContClassName={'scrollH'}>
       <p className="pop_tit">전자 세금계산서 신청</p>
-      <div className="pop_cont_scroll tax_invoice2" style={{ height: '600px' }}>
+      <div className="pop_cont_scroll tax_invoice2" style={{ height: '550px' }}>
         {!processDone ? (
           <>
             <form onSubmit={submit}>
