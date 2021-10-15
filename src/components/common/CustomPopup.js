3bcb73864f4a9ac69a87f1c2228d65980abc434e
kimjongkeun <kjkandrea@gmail.com>
2021-10-15 18:06:25 +0900

    Add popup base structure

--- src/components/common/CustomPopup.js
+++ src/components/common/CustomPopup.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getDisplayPopups, getDisplayPopupsPopupNos } from '../../api/display';
 
 const CustomPopup = () => {
+  const [popups, setPopups] = useState([]);
+
+  const init = () => {
+    fetchPopupNos().
+      then(nos => nos.toString()).
+      then(fetchPopups).
+      then(setPopups);
+  };
+
+  useEffect(init, []);
+
+  function fetchPopupNos () {
+    const map = data => data.map(({ popupNo }) => popupNo);
+
+    return getDisplayPopups().then(({ data }) => data).then(map);
+  }
+
+  function fetchPopups (no) {
+    return getDisplayPopupsPopupNos(no).then(({ data }) => data);
+  }
+
   return (
-    <h1 style={{ padding: '100px', fontSize: '72px' }}>나는 팝업이다.</h1>
+    <div style={{ padding: '100px', maxWidth: '100%' }}>
+      {popups.map(popup => <Popup popup={popup} />)}
+    </div>
   );
 };
 
 export default CustomPopup;
+
+const Popup = ({ popup }) => {
+  return (
+    <p>{JSON.stringify(popup)}</p>
+  );
+};
