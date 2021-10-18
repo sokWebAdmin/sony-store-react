fea318ad265fcdc5d0e539d1c23e6fae3af84142
kimjongkeun <kjkandrea@gmail.com>
2021-10-18 12:08:00 +0900

    Add style

--- src/components/common/customPopup/partials/LayerPopup.js
+++ src/components/common/customPopup/partials/LayerPopup.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useMemo, useRef } from 'react';
 
+import '../../../../assets/scss/partials/customPopup.scss';
+
 const LayerPopup = ({ popup }) => {
 
   const contentArea = useRef();
@@ -21,12 +23,16 @@ const LayerPopup = ({ popup }) => {
 
   return (
     <>
-      <div id="popup-test" className="layer_mask" tabIndex="0"
+      <div className="layer_mask" tabIndex="0"
            style={{ display: 'block' }} />
-      <div className='popup_wrap' style={{ display: 'block', width: 'auto' }}>
+      <div className='popup_wrap custom_popup_layer'
+           style={{ display: 'block', width: 'auto' }}>
         <div className="pop_inner">
-          <div className='pop_cont' ref={contentArea} style={sizeStyle} />
-          <button type="button" className="ico_x closed delete" title="팝업창 닫기">
+          <div className='pop_cont custom_popup_layer--container'
+               ref={contentArea} style={sizeStyle} />
+          <button type="button"
+                  className="ico_x closed delete custom_popup_layer--delete"
+                  title="팝업창 닫기">
             <span className="delete">팝업창 닫기</span>
           </button>
         </div>
