a440079ab58eefb6849f8486a3d680855dd79efa
kimjongkeun <kjkandrea@gmail.com>
2021-10-18 11:39:11 +0900

    Add markup

--- src/components/common/customPopup/partials/Popup.js
+++ src/components/common/customPopup/partials/Popup.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
+//
+import LayerPopup from '../../LayerPopup';
+
 const Popup = ({ popup }) => {
   return (
-    <p>{JSON.stringify(popup)}</p>
+    <>
+      <div id="popup-test" className="layer_mask" tabIndex="0"
+           style={{ display: 'block' }} />
+      <div className='popup_wrap' style={{ display: 'block' }}>
+        <div className="pop_inner">
+          <div className='pop_cont'>
+            {JSON.stringify(popup)}
+          </div>
+          <button type="button" className="ico_x closed delete" title="팝업창 닫기">
+            <span className="delete">팝업창 닫기</span>
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
