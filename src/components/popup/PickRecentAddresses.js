79efd4670a455ca7b4349e287f51768464eb75ee
KwonYG <andamirocll@gmail.com>
2021-10-20 11:06:07 +0900

    fix: mobile address popup height

--- src/components/popup/PickRecentAddresses.js
+++ src/components/popup/PickRecentAddresses.js
@@ -1,14 +1,14 @@
 import { useState, useEffect, useMemo } from 'react';
-
+import { isMobile } from 'react-device-detect';
 import LayerPopup from '../common/LayerPopup';
 
 const PickRecentAddresses = ({ shipping, setShipping, recentAddresses, close }) => {
-
   const [pickAddressNo, setPickAddressNo] = useState(shipping.addressNo);
 
   const pickAddress = useMemo(
     () => recentAddresses.find(({ addressNo }) => addressNo === pickAddressNo),
-    [pickAddressNo, recentAddresses]);
+    [pickAddressNo, recentAddresses],
+  );
 
   useEffect(() => {
     if (!recentAddresses.length || shipping?.addressNo) {
@@ -32,47 +32,50 @@ const PickRecentAddresses = ({ shipping, setShipping, recentAddresses, close }) 
   };
 
   return (
-    <LayerPopup
-      size={'m'}
-      popContClassName={'shipping_addr'}
-      onClose={close}
-    >
+    <LayerPopup size={'m'} popContClassName={`shipping_addr ${isMobile && 'scrollH'}`} onClose={close}>
       <p className="pop_tit">최근 배송지</p>
       <p className="pop_txt">최근에 입력하신 배송지 5개까지 노출됩니다.</p>
-      <div className="chk_select_zone">
-        <ul className="chk_select_inner">
-          {
-            recentAddresses.length ? recentAddresses.map(address => (
-              <li className="chk_select_list" key={address.addressNo}
-                  onClick={() => setPickAddressNo(address.addressNo)}>
-                <div
-                  className="chk_select_item label_click">
-                  <div className="radio_box radio_only chk_select">
-                    <input type="radio" className="inp_radio"
-                           id={address.addressNo}
-                           name="addr_chk1"
-                           checked={pickAddressNo === address.addressNo}
-                           onChange={() => setPickAddressNo(
-                             address.addressNo)} />
-                    <label htmlFor={address.addressNo}
-                           className="contentType"></label>
+      <div className="pop_cont_scroll" style={{ height: isMobile ? '400px' : undefined }}>
+        <div className="chk_select_zone">
+          <ul className="chk_select_inner">
+            {recentAddresses.length ? (
+              recentAddresses.map((address) => (
+                <li
+                  className="chk_select_list"
+                  key={address.addressNo}
+                  onClick={() => setPickAddressNo(address.addressNo)}
+                >
+                  <div className="chk_select_item label_click">
+                    <div className="radio_box radio_only chk_select">
+                      <input
+                        type="radio"
+                        className="inp_radio"
+                        id={address.addressNo}
+                        name="addr_chk1"
+                        checked={pickAddressNo === address.addressNo}
+                        onChange={() => setPickAddressNo(address.addressNo)}
+                      />
+                      <label htmlFor={address.addressNo} className="contentType"></label>
+                    </div>
+                    <div className="chk_select_info">
+                      <p className="txt">
+                        <strong className="name">{address.receiverName}</strong>
+                        <span className="number">{address.receiverContact1}</span>
+                      </p>
+                      <p className="txt">{address.receiverAddress}</p>
+                    </div>
                   </div>
-                  <div className="chk_select_info">
-                    <p className="txt">
-                      <strong className="name">{address.receiverName}</strong>
-                      <span className="number">{address.receiverContact1}</span>
-                    </p>
-                    <p className="txt">{address.receiverAddress}</p>
-                  </div>
-                </div>
-              </li>
-            )) : <h1>최근에 등록하신 배송지가 없습니다.</h1>
-          }
-        </ul>
+                </li>
+              ))
+            ) : (
+              <h1>최근에 등록하신 배송지가 없습니다.</h1>
+            )}
+          </ul>
+        </div>
       </div>
       <div className="btn_article">
-        <button className="button button_positive button-m" type="button"
-                onClick={submit}>확인
+        <button className="button button_positive button-m" type="button" onClick={submit}>
+          확인
         </button>
       </div>
     </LayerPopup>
