5d193d002d1fcbfaa295b2fa672718b269b55798
BoMeeYoon <89110544+nanuya@users.noreply.github.com>
2021-10-21 08:43:42 +0900

    update benefit (#825)

--- src/components/products/viewTopContent/Benefits.js
+++ src/components/products/viewTopContent/Benefits.js
@@ -10,27 +10,30 @@ export default function Benefits({ price }) {
   // const { discount } = getPricePerProduct(price);
 
   const unit = mallInfo?.accumulationUnit;
+  const nonProfit = price.accumulationRate === 0;
   
   return (
     <div className="cont line">
-      <p className="tit">회원별 마일리지 적립혜택 <span className="icon_question">!</span></p>
-      <ul className="membership_rating">
-        {
-          mallInfo?.mall?.grades.filter(({ reserveBenefit }) => reserveBenefit.reserveRate > 0).map((g, idx) => {
-            const UPPERCASE = g.label.toUpperCase();
-            const LOWERCASE = g.label.toLowerCase();
-            return (
-              <li key={`${g.label}${idx}`} className={g.label === 'Membership' ? 'family' : LOWERCASE}>{/* class 별 등급 색상 지정 vvip / vip / family */}
-                <span className="mark">{UPPERCASE.includes('VIP') ? UPPERCASE.split('IP')[0] : UPPERCASE}</span>
-                <div className="save_info">
-                  <span className="percentage">{UPPERCASE} {g.reserveBenefit.reserveRate}%</span>
-                  <p className="mileage"><span className="num">{wonComma(discount * (g.reserveBenefit.reserveRate/100))}</span> {unit}</p>
-                </div>
-              </li>
-            )
-          })
-        }
-      </ul>
+      <p className="tit">{nonProfit ? '본 상품은 적립금이 적립되지 않는 상품입니다.' : '회원별 마일리지 적립혜택 '}<span className="icon_question">!</span></p>
+      {
+        !nonProfit && <ul className="membership_rating">
+                        {
+                          mallInfo?.mall?.grades.filter(({ reserveBenefit }) => reserveBenefit.reserveRate > 0).map((g, idx) => {
+                            const UPPERCASE = g.label.toUpperCase();
+                            const LOWERCASE = g.label.toLowerCase();
+                            return (
+                              <li key={`${g.label}${idx}`} className={g.label === 'Membership' ? 'family' : LOWERCASE}>{/* class 별 등급 색상 지정 vvip / vip / family */}
+                                <span className="mark">{UPPERCASE.includes('VIP') ? UPPERCASE.split('IP')[0] : UPPERCASE}</span>
+                                <div className="save_info">
+                                  <span className="percentage">{UPPERCASE} {g.reserveBenefit.reserveRate}%</span>
+                                  <p className="mileage"><span className="num">{wonComma(discount * (g.reserveBenefit.reserveRate/100))}</span> {unit}</p>
+                                </div>
+                              </li>
+                            )
+                          })
+                        }
+                      </ul>
+      }
     </div>
   )
 }
