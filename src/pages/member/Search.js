66d6d9b6e17aee8995f611df73e4fe34ba5cf35a
kimeunbi <ebkim@godo.co.kr>
2021-10-21 17:16:42 +0900

    Add input value space removal

--- src/pages/member/Search.js
+++ src/pages/member/Search.js
@@ -73,7 +73,7 @@ export default function Search() {
     }
 
     return validation;
-  }
+  };
 
   const searchMember = async () => {
     if (!authSent || !authCheck) {
@@ -85,7 +85,7 @@ export default function Search() {
       const { data: response } = await getMemberInfo({
         type: '10',
         firstname: searchValue.memberName,
-        mobile: searchValue.mobileNo
+        mobile: searchValue.mobileNo,
       });
       if (response.errorCode === '0000') {
         setResult(response.body.customerid);
@@ -97,7 +97,7 @@ export default function Search() {
       const { data: response } = await getMemberInfo({
         type: '20',
         customerid: searchValue.email,
-        mobile: searchValue.mobileNo
+        mobile: searchValue.mobileNo,
       });
       if (response.errorCode === '0000') {
         openAlert(`${response.body.customerid} 메일을 보냈습니다.`, true);
@@ -169,123 +169,194 @@ export default function Search() {
         <div className="container" id="container">
           <div className="login">
             <h2 className="login__title">아이디 · 비밀번호 찾기</h2>
-            <p className="login__desc">아이디·비밀번호가 기억나지 않으신가요? <span>등록하신 정보로 아이디와 비밀번호를 찾으실 수 있습니다.</span></p>
+            <p className="login__desc">
+              아이디·비밀번호가 기억나지 않으신가요?{' '}
+              <span>등록하신 정보로 아이디와 비밀번호를 찾으실 수 있습니다.</span>
+            </p>
             <div className="login__search_box">
               <ul className="login__tab search_type">
-                <li className={tabState === 'id' ? 'current' : ''}><a href="javascript:void(0)" onClick={() => setTabState('id')}>아이디 찾기</a></li>
-                <li className={tabState === 'password' ? 'current' : ''}><a href="javascript:void(0)" onClick={() => setTabState('password')}>비밀번호
-                  찾기</a></li>
+                <li className={tabState === 'id' ? 'current' : ''}>
+                  <a href="javascript:void(0)" onClick={() => setTabState('id')}>
+                    아이디 찾기
+                  </a>
+                </li>
+                <li className={tabState === 'password' ? 'current' : ''}>
+                  <a href="javascript:void(0)" onClick={() => setTabState('password')}>
+                    비밀번호 찾기
+                  </a>
+                </li>
               </ul>
 
               <div className="tabResult">
-                {!result ? <div className="result_cont tab2 on">
+                {!result ? (
+                  <div className="result_cont tab2 on">
                     <div className={`group ${emptySearchValue ? 'error' : ''}`}>
                       <div className="inp_box">
                         <label className="inp_desc" htmlFor="loginName">
-                          <input type="text" id="loginName" className="inp" placeholder="&nbsp;" autoComplete="off"
-                                 value={tabState === 'id' ? searchValue.memberName : searchValue.email} tabIndex={1}
-                                 onChange={(e) => onChangeValues(tabState === 'id' ? 'memberName' : 'email', e.target.value)}
+                          <input
+                            type="text"
+                            id="loginName"
+                            className="inp"
+                            placeholder="&nbsp;"
+                            autoComplete="off"
+                            value={tabState === 'id' ? searchValue.memberName : searchValue.email}
+                            tabIndex={1}
+                            onChange={(e) =>
+                              onChangeValues(tabState === 'id' ? 'memberName' : 'email', e.target.value.trim())
+                            }
                           />
-                          {tabState === 'id' ?
-                            <span className="label">이름<span>(띄어쓰기 없이 입력하세요.)</span></span> :
-                            <span className="label">이메일 아이디<span>(예 : sony@sony.co.kr)</span></span>}
+                          {tabState === 'id' ? (
+                            <span className="label">
+                              이름<span>(띄어쓰기 없이 입력하세요.)</span>
+                            </span>
+                          ) : (
+                            <span className="label">
+                              이메일 아이디<span>(예 : sony@sony.co.kr)</span>
+                            </span>
+                          )}
                           <span className="focus_bg" />
                         </label>
                       </div>
-                      <div className="error_txt"><span className="ico" />이름을 입력해 주세요. (띄어쓰기 없이 입력하세요.)</div>
+                      <div className="error_txt">
+                        <span className="ico" />
+                        이름을 입력해 주세요. (띄어쓰기 없이 입력하세요.)
+                      </div>
                     </div>
                     <div className={`group btn_type ${emptyMobile ? 'error' : ''}`}>
                       <div className="inp_box">
                         <label className="inp_desc" htmlFor="phonenumber">
-                          <input type="text" id="phonenumber" className="inp" placeholder=" " autoComplete="off"
-                                 tabIndex={2} value={searchValue.mobileNo}
-                                 onChange={(e) => onChangeValues('mobileNo', e.target.value)}
+                          <input
+                            type="text"
+                            id="phonenumber"
+                            className="inp"
+                            placeholder=" "
+                            autoComplete="off"
+                            tabIndex={2}
+                            value={searchValue.mobileNo}
+                            onChange={(e) => onChangeValues('mobileNo', e.target.value.trim())}
                           />
-                          <span className="label">휴대폰 번호<span>(-없이 입력하세요.)</span></span>
+                          <span className="label">
+                            휴대폰 번호<span>(-없이 입력하세요.)</span>
+                          </span>
                           <span className="focus_bg" />
                         </label>
                         <div className="btn_box">
-                          {(authSent && authCheck == false) ?
-                            <button type="button" className={`btn btn_default`} onClick={() => {
-                              if (authAvailable === true) {
-                                //유효기간 설정
-                                let now = new Date().getTime();
-                                const target = new Date(now + (3 * 60000));
-                                setTime(179);
-                                setExpireAt(target);
+                          {authSent && authCheck == false ? (
+                            <button
+                              type="button"
+                              className={`btn btn_default`}
+                              onClick={() => {
+                                if (authAvailable === true) {
+                                  //유효기간 설정
+                                  let now = new Date().getTime();
+                                  const target = new Date(now + 3 * 60000);
+                                  setTime(179);
+                                  setExpireAt(target);
 
-                                //인증번호 발송
-                                _sendSMS(searchValue.mobileNo);
-                              }
-                            }}>재전송</button>
-                            :
-                            <button type="button"
-                                    className={`btn ${(authAvailable == true && authCheck == false) ? 'btn_primary' : 'btn_disable'}`}
-                                    onClick={() => {
-                                      if (authAvailable === true) {
-                                        //유효기간 설정
-                                        let now = new Date().getTime();
-                                        const target = new Date(now + (3 * 60000));
-                                        setTime(179);
-                                        setExpireAt(target);
+                                  //인증번호 발송
+                                  _sendSMS(searchValue.mobileNo);
+                                }
+                              }}
+                            >
+                              재전송
+                            </button>
+                          ) : (
+                            <button
+                              type="button"
+                              className={`btn ${
+                                authAvailable == true && authCheck == false ? 'btn_primary' : 'btn_disable'
+                              }`}
+                              onClick={() => {
+                                if (authAvailable === true) {
+                                  //유효기간 설정
+                                  let now = new Date().getTime();
+                                  const target = new Date(now + 3 * 60000);
+                                  setTime(179);
+                                  setExpireAt(target);
 
-                                        //인증번호 발송
-                                        _sendSMS(searchValue.mobileNo);
-                                      }
-                                    }}>인증번호</button>
-                          }
+                                  //인증번호 발송
+                                  _sendSMS(searchValue.mobileNo);
+                                }
+                              }}
+                            >
+                              인증번호
+                            </button>
+                          )}
                         </div>
                       </div>
-                      <div className="error_txt"><span className="ico" />휴대폰 번호를 입력해주세요. (-없이 입력하세요.)</div>
+                      <div className="error_txt">
+                        <span className="ico" />
+                        휴대폰 번호를 입력해주세요. (-없이 입력하세요.)
+                      </div>
                     </div>
-                    {
-                      authSent === true &&
+                    {authSent === true && (
                       <div className="group btn_type">
                         <div className="inp_box">
                           <label className="inp_desc" htmlFor="certifynumber">
-                            <input type="text" id="certifynumber" className="inp" placeholder=" " autoComplete="off"
-                                   tabIndex={6} value={authCode} onChange={(e) => {
-                              setAuthCode(e.target.value);
-                            }} readOnly={authCheck} />
+                            <input
+                              type="text"
+                              id="certifynumber"
+                              className="inp"
+                              placeholder=" "
+                              autoComplete="off"
+                              tabIndex={6}
+                              value={authCode}
+                              onChange={(e) => {
+                                setAuthCode(e.target.value);
+                              }}
+                              readOnly={authCheck}
+                            />
                             <span className="label">인증번호</span>
-                            {authCheck === false &&
-                            <span className="timer" id="timer">{timeFormat(time)}</span>
-                            }
+                            {authCheck === false && (
+                              <span className="timer" id="timer">
+                                {timeFormat(time)}
+                              </span>
+                            )}
                             <span className="focus_bg" />
                           </label>
                           <div className="btn_box">
-                            <button type="button" className={`btn ${authCheck !== true ? 'btn_primary' : 'btn_disable'}`}
-                                    onClick={() => {
-                                      if (authCheck !== true) {
-                                        if (time === 0) {
-                                          openAlert('인증시간이 만료되었습니다. 재전송 후 인증해주세요.');
-                                        } else {
-                                          if (authCode === '') {
-                                            openAlert('인증번호를 입력해주세요.');
-                                            return;
-                                          }
-                                          _verifySMS(searchValue.mobileNo, authCode, 'JOIN');
-                                        }
-                                      }
-                                    }}>인증
+                            <button
+                              type="button"
+                              className={`btn ${authCheck !== true ? 'btn_primary' : 'btn_disable'}`}
+                              onClick={() => {
+                                if (authCheck !== true) {
+                                  if (time === 0) {
+                                    openAlert('인증시간이 만료되었습니다. 재전송 후 인증해주세요.');
+                                  } else {
+                                    if (authCode === '') {
+                                      openAlert('인증번호를 입력해주세요.');
+                                      return;
+                                    }
+                                    _verifySMS(searchValue.mobileNo, authCode, 'JOIN');
+                                  }
+                                }
+                              }}
+                            >
+                              인증
                             </button>
                           </div>
                         </div>
                         <div className="certify_txt">※ 입력하신 번호로 인증번호가 전송되었습니다.</div>
                       </div>
-                    }
+                    )}
                     <div className="btn_box full">
-                      <button type="button" className="btn btn_dark" onClick={() => searchMember()}>확인</button>
+                      <button type="button" className="btn btn_dark" onClick={() => searchMember()}>
+                        확인
+                      </button>
                     </div>
-                  </div> :
+                  </div>
+                ) : (
                   <div className="id_result">
                     <span className="ico_id_result"></span>
                     <p className="info">회원님의 이메일 아이디 검색 결과입니다.</p>
                     <p className="mailaddress">{result}</p>
                     <div className="btn_box full">
-                      <Link to={'/member/login'} className="btn btn_dark">로그인</Link>
+                      <Link to={'/member/login'} className="btn btn_dark">
+                        로그인
+                      </Link>
                     </div>
-                  </div>}
+                  </div>
+                )}
               </div>
               <div className="caution_txt">
                 <p>· 가입하신 정보와 일치하지 않을 경우 검색이 안될 수 있습니다.</p>
