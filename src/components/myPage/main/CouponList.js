06b59f53f957b4a733d8c24b274a3baadb17db4b
KwonYG <andamirocll@gmail.com>
2021-10-21 10:03:28 +0900

    fix: coupon load more count

--- src/components/myPage/main/CouponList.js
+++ src/components/myPage/main/CouponList.js
@@ -41,7 +41,7 @@ const CouponList = () => {
 
   // 다음 페이지가 없는 경우 loadmore 버튼 숨김
   const showLoadMoreBtn = (newCoupons) => {
-    if (newCoupons.length < 0) {
+    if (newCoupons.length < 10) {
       setLoadMoreBtnVisible(false);
       return;
     }
