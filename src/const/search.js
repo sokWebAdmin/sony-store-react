d42d02dc484aafb5ac676bda6088584ed4edf7f6
kimeunbi <ebkim@godo.co.kr>
2021-10-22 16:58:45 +0900

    Add old product sorting

--- src/const/search.js
+++ src/const/search.js
@@ -3,14 +3,14 @@ export const TAB_MAP = {
   PRODUCT: '제품',
   EVENT: '기획전',
   CATEGORY: '카테고리',
-  NOTICE: '공지사항'
+  NOTICE: '공지사항',
 };
 
 export const orderList = [
   {
-    orderBy: 'MD_RECOMMEND',
+    orderBy: 'RECENT_PRODUCT',
     label: '최신순',
-    query: { 'order.by': 'MD_RECOMMEND', 'order.direction': 'ASC' },
+    query: { 'order.by': 'RECENT_PRODUCT', 'order.direction': 'ASC' },
   },
   {
     orderBy: 'DISCOUNTED_PRICE',
@@ -22,11 +22,16 @@ export const orderList = [
     label: '낮은 가격순',
     query: { 'order.by': 'DISCOUNTED_PRICE', 'order.direction': 'ASC' },
   },
+  {
+    orderBy: 'OLD_PRODUCT',
+    label: '오래된 순',
+    query: { 'order.by': 'RECENT_PRODUCT', 'order.direction': 'ASC' },
+  },
 ];
 
 export const PAGE_SIZE = {
   PRODUCT: 9,
   EVENT: 12,
   CATEGORY: 10,
-  NOTICE: 10
-}
+  NOTICE: 10,
+};
