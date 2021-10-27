756b11187b22faf4879185126a2068cdbd6c26c8
BoMeeYoon <89110544+nanuya@users.noreply.github.com>
2021-10-27 15:51:35 +0900

    [QA] 검색결과 수정 (#922)
    * fix search result
    
    * fix import/first error
    
    * sfix search result style
    
    * add https to footer for omniture

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext, useCallback, useMemo } from 'react';
 import { Switch, useLocation, Route, useHistory } from 'react-router-dom';
-import { throttle, debounce } from 'lodash';
+import { throttle, debounce, curry } from 'lodash';
 
 //Component
 import Header from './components/Header';
@@ -111,10 +111,8 @@ import CustomPopup from './components/common/customPopup/CustomPopup';
 import { getDisplayPopups, getDisplayPopupsPopupNos } from './api/display';
 import AppBar from './components/app/AppBar';
 
-import { openBrowser } from './utils/openBrowser.js'
-import{ curry } from 'lodash'
+import { openBrowser } from './utils/openBrowser.js';
 
-console.log('is devstore');
 
 const App = (props) => {
   const agent = getAgent();
@@ -129,8 +127,8 @@ const App = (props) => {
   const categoryDispatch = useCategoryDispatch();
 
   useEffect(() => {
-    window['anchorProtocol'] = 'https://'
-    window['openBrowser'] = curry(openBrowser)(agent)
+    window['anchorProtocol'] = 'https://';
+    window['openBrowser'] = curry(openBrowser)(agent);
   }, [agent]);
 
   useEffect(() => {
@@ -141,8 +139,7 @@ const App = (props) => {
   }, [dispatch, state?.mall]);
 
   const getMallInfo = async () => {
-    if (window.location.pathname === '/callback' || window.location.pathname ===
-      '/member/joinStep') {
+    if (window.location.pathname === '/callback' || window.location.pathname === '/member/joinStep') {
       return;
     }
     if (isLogin) {
@@ -241,10 +238,10 @@ const App = (props) => {
       return;
     }
 
-    fetchPopupNos().
-      then((nos) => nos.toString()).
-      then(fetchPopups).
-      then((res) => {
+    fetchPopupNos()
+      .then((nos) => nos.toString())
+      .then(fetchPopups)
+      .then((res) => {
         setPopups(res);
       });
   }, [location]);
@@ -252,13 +249,7 @@ const App = (props) => {
   Array();
 
   const isAppBarEnabled = useMemo(() => {
-    const rejectPathNames = [
-      '/product-view',
-      '/cart',
-      '/order/sheet',
-      '/gift/sheet',
-      '/order/complete',
-      '/app/terms'];
+    const rejectPathNames = ['/product-view', '/cart', '/order/sheet', '/gift/sheet', '/order/complete', '/app/terms'];
     return (
       agent.isApp &&
       ['android', 'ios'].some((v) => v === agent.device) &&
