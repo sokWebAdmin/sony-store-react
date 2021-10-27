c1d7c664007fe6aec7193dfebb72d37ab980ca0f
kimjongkeun <kjkandrea@gmail.com>
2021-10-27 10:23:11 +0900

    Fix membername

--- src/pages/mypage/MyPageMain.js
+++ src/pages/mypage/MyPageMain.js
@@ -89,7 +89,7 @@ export default function MyPageMain() {
     return my?.totalExpireMileage || 0;
   }, [my]);
 
-  const memberName = useMemo(() => my?.firstName, [my]);
+  const memberName = useMemo(() => my?.firstname ?? profile?.memberName, [my]);
 
   return (
     <>
