8ce7f19143da6519d208d3c31f6078f309a4beaf
andrea <kjkandrea@gmail.com>
2021-10-28 12:30:57 +0900

    Add test console

--- src/App.js
+++ src/App.js
@@ -113,6 +113,8 @@ import AppBar from './components/app/AppBar';
 
 import { openBrowser, openWindow } from './utils/openBrowser.js';
 
+console.log('hello real')
+
 const App = (props) => {
   const agent = getAgent();
   const history = useHistory();
