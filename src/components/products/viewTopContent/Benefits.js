b35306ab80dbd7511ec5bf2d379cab95df97007e
nanuya <nanuya0331@gmail.com>
2021-11-05 15:18:00 +0900

    fix benefit-real

--- src/components/products/viewTopContent/Benefits.js
+++ src/components/products/viewTopContent/Benefits.js
@@ -3,11 +3,12 @@ import { getPricePerProduct } from '../../../utils/product';
 import { wonComma } from '../../../utils/utils';
 
 // 할인 혜택
-export default function Benefits({ price }) {
+export default function Benefits({ price, accumulationUseYn }) {
   const mallInfo = useMallState();
   const discount = price.salePrice;
   // const { discount } = getPricePerProduct(price);
 
+  const useAcc = accumulationUseYn === 'Y';
   const unit = mallInfo?.accumulationUnit;
   const nonProfit = price.accumulationRate === 0;
   const hasSpecialProfit = price.accumulationRate > 0;
@@ -18,14 +19,14 @@ export default function Benefits({ price }) {
         {nonProfit ? '본 상품은 적립금이 적립되지 않는 상품입니다.' : '회원별 마일리지 적립혜택 '}
         <span className="icon_question">!</span>
       </p>
-      {!nonProfit && (
+      {useAcc && !nonProfit && (
         <ul className="membership_rating">
           {mallInfo?.mall?.grades
             .filter(({ reserveBenefit, used }) => reserveBenefit.reserveRate > 0 && used === true)
             .map((g, idx) => {
               const UPPERCASE = g.label.toUpperCase();
               const LOWERCASE = g.label.toLowerCase();
-              const _profit = hasSpecialProfit ? price.accumulationRate : g.reserveBenefit.reserveRate;
+              const _profit = useAcc && hasSpecialProfit ? price.accumulationRate : g.reserveBenefit.reserveRate;
               return (
                 <li key={`${g.label}${idx}`} className={g.label === 'Membership' ? 'family' : LOWERCASE}>
                   {/* class 별 등급 색상 지정 vvip / vip / family */}
