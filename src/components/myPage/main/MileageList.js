c178b648cb53852b442c690f926cca68bc84d1f7
Jerome <jerome@geekstudio.co.kr>
2022-01-13 00:19:11 +0900

    refactor: MileageList Component 리팩토링
     - 기존에 list를 받아서 리스트 재가공하는 작업 삭제

--- src/components/myPage/main/MileageList.js
+++ src/components/myPage/main/MileageList.js
@@ -1,69 +1,52 @@
-import React, { useMemo } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import dayjs from 'dayjs';
 import { toCurrencyString } from 'utils/unit';
 import { getMileageDetail } from 'utils/mileage';
 
 const MileageList = ({ list }) => {
-    const mileages = useMemo(
-        () =>
-            list.map(
-                ({
-                    sysRegDtime,
-                    expiredDateTime,
-                    amount,
-                    mappingKey,
-                    extraData,
-                    type,
-                }) => ({
-                    regiDate: sysRegDtime
-                        ? sysRegDtime.substr(0, 10).replace(/-/g, '.')
-                        : '-',
-                    expiredDate: expiredDateTime
-                        ? expiredDateTime.substr(0, 10).replace(/-/g, '.')
-                        : '-',
-                    extraData,
-                    mappingKey,
-                    detail: getMileageDetail(type),
-                    amount: amount,
-                    isMinus: ['11', '20', 11, 20].includes(type),
-                    amountClassList: ['11', '20', 11, 20].includes(type)
-                        ? 'col_table_cell order_mileage down'
-                        : 'col_table_cell order_mileage up',
-                }),
-            ),
-        [list],
-    );
-
     return (
         <div className='col_table_body'>
-            {mileages.map((item, idx) => (
-                <div className='col_table_row' key={idx}>
+            {list.map((item, index) => (
+                <div className='col_table_row' key={index}>
                     <div className='col_table_cell order_date'>
-                        <p className='txt'>{item.regiDate}</p>
+                        <p className='txt'>
+                            {item.sysRegDtime
+                                ? dayjs(item.sysRegDtime).format('YYYY.MM.DD')
+                                : '-'}
+                        </p>
                     </div>
                     <div className='col_table_cell order_details'>
-                        <p className='txt'>{item.detail}</p>
+                        <p className='txt'>{getMileageDetail(item.type)}</p>
                     </div>
                     <div className='col_table_cell order_number'>
                         {item.mappingKey && item.mappingKey !== 'null' && (
-                            <Link
-                                to={`/my-page/order-detail?orderNo=${item.mappingKey}`}
-                                className='txt'
-                            >
-                                {item.mappingKey}
-                            </Link>
+                            <span className='txt'>{item.mappingKey}</span>
                         )}
                     </div>
-                    <div className={item.amountClassList}>
+                    <div
+                        className={
+                            ['11', '20', 11, 20].includes(item.type)
+                                ? 'col_table_cell order_mileage down'
+                                : 'col_table_cell order_mileage up'
+                        }
+                    >
                         <p className='txt'>
-                            {item.isMinus ? '- ' : '+ '}{' '}
+                            {['11', '20', 11, 20].includes(item.type)
+                                ? '- '
+                                : '+ '}{' '}
                             {item?.amount
                                 ? toCurrencyString(item.amount).replace('-', '')
                                 : 'N'}
                         </p>
                     </div>
                     <div className='col_table_cell order_expiration'>
-                        <p className='txt'>{item.expiredDate}</p>
+                        <p className='txt'>
+                            {item.expiredDate
+                                ? dayjs(item.expiredDateTime).format(
+                                      'YYYY.MM.DD',
+                                  )
+                                : '-'}
+                        </p>
                     </div>
                 </div>
             ))}
