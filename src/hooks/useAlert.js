5799dab6dc546f3a7b59ebe22a0d6490208fc355
Jerome <jerome@geekstudio.co.kr>
2022-01-14 12:36:16 +0900

    fix: 마이페이지 알림수신 텍스트 오류 수정
     - 텍스트 오류 수정
     - 코드에 prettier 적용
     - memberGrade 변수 constants.js 파일로 이동

--- src/hooks/useAlert.js
+++ src/hooks/useAlert.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+// TODO: derived state를 사용하고 있으므로 source of truth가 아님!
 export const useAlert = ({ visible = false, message = '' }) => {
     const [alertVisible, setAlertVisible] = useState(visible);
     const [alertMessage, setAlertMessage] = useState(message);
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,11 @@
 export const breakPoint = 1280;
 
 export const breakPointTablet = 640;
+
+// vvip / vip / family
+export const memberGrade = {
+    N: { className: 'family', label: 'MEMBERSHIP' }, // 일반도 MEMBERSHIP
+    M: { className: 'family', label: 'MEMBERSHIP' },
+    V: { className: 'vip', label: 'VIP' },
+    VV: { className: 'vvip', label: 'VVIP' },
+};
