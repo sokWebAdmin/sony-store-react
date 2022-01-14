ca2450feb729049b6bd07e910db3e72dbfe7e382
Jerome <jerome@geekstudio.co.kr>
2022-01-14 16:18:19 +0900

    refactor: CouponListItem Component 리팩토링
     - 날짜변환 dayjs를 사용하도록 변경
     - fixedAmt 값에 따라 할인율 / 할인액이 나오도록 수정
     - PropTypes 추가

--- src/components/myPage/main/CouponListItem.js
+++ src/components/myPage/main/CouponListItem.js
@@ -1,31 +1,64 @@
-import { toCurrencyString } from '../../../utils/unit';
+import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
+import { toCurrencyString } from 'utils/unit';
 
-const CouponListItem = ({ couponIssueNo, couponName, discountRate, minSalePrice, issueYmdt, useEndYmdt }) => {
-  const getYmd = (ymdt) => {
-    return ymdt.split(' ')[0];
-  };
-
-  return (
-    <div className="coupon_box">
-      <span className="coupon_type">COUPON</span>
-      <div className="coupon">
-        <div className="coupon_head">
-          <span className="coupon_no">
-            No. <span className="num">{couponIssueNo}</span>
-          </span>
-          <p className="tit">
-            <span dangerouslySetInnerHTML={{ __html: couponName }} />
-            <span className="percentage">{discountRate}%</span> 할인
-          </p>
-          <p className="cut_txt">{toCurrencyString(minSalePrice)}원 이상 구매 시 </p>
+const CouponListItem = ({
+    couponName,
+    fixedAmt,
+    discountRate,
+    discountAmt,
+    minSalePrice,
+    issueYmdt,
+    useEndYmdt,
+}) => {
+    return (
+        <div className='coupon_box'>
+            <span className='coupon_type'>COUPON</span>
+            <div className='coupon'>
+                <div className='coupon_head'>
+                    <div className='tit'>
+                        <div dangerouslySetInnerHTML={{ __html: couponName }} />
+                        {!fixedAmt && (
+                            <p>
+                                <span className='percentage'>
+                                    {discountRate}%
+                                </span>{' '}
+                                할인
+                            </p>
+                        )}
+                        {fixedAmt && (
+                            <p>
+                                <span className='percentage'>
+                                    {discountAmt}원
+                                </span>{' '}
+                                할인
+                            </p>
+                        )}
+                    </div>
+                    <p className='cut_txt'>
+                        {toCurrencyString(minSalePrice)}원 이상 구매 시
+                    </p>
+                </div>
+                <p className='expiration_txt'>
+                    <strong>유효 기간 </strong>
+                    {`${dayjs(issueYmdt).format('YYYY-MM-DD')} ~ ${dayjs(
+                        useEndYmdt,
+                    ).format('YYYY-MM-DD')}`}
+                </p>
+            </div>
         </div>
-        <p className="expiration_txt">
-          <strong>유효 기간 </strong>
-          {getYmd(issueYmdt)} ~ {getYmd(useEndYmdt)}
-        </p>
-      </div>
-    </div>
-  );
+    );
+};
+
+CouponListItem.propTypes = {
+    couponIssueNo: PropTypes.number.isRequired,
+    couponName: PropTypes.string.isRequired,
+    fixedAmt: PropTypes.bool.isRequired,
+    discountRate: PropTypes.number,
+    discountAmt: PropTypes.number,
+    minSalePrice: PropTypes.number,
+    issueYmdt: PropTypes.string,
+    useEndYmdt: PropTypes.string,
 };
 
 export default CouponListItem;
