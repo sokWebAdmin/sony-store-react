20b0169da0d62c91d52d7758f20c67f118b8da21
Jerome <jerome@geekstudio.co.kr>
2022-01-18 15:54:20 +0900

    refactor: MemberSummary Component 리팩토링
     - 코드 스타일 정리
     - memberGrade 변수 삭제 (utils에 있는 변수임)

--- src/components/myPage/main/MemberSummary.js
+++ src/components/myPage/main/MemberSummary.js
@@ -1,91 +1,110 @@
 import { useEffect, useMemo, useState } from 'react';
-
 import { Link } from 'react-router-dom';
-import { toCurrencyString } from '../../../utils/unit';
-import { getCouponsSummary } from '../../../api/promotion';
-import { syncCoupon } from '../../../api/sony/coupon';
+import { toCurrencyString } from 'utils/unit';
+import { getCouponsSummary } from 'api/promotion';
+import { syncCoupon } from 'api/sony/coupon';
+import { memberGrade } from 'utils/constants';
 
-// vvip / vip / family
-const memberGrade = {
-  일반: { className: 'family', label: 'MEMBERSHIP' }, // 일반도 MEMBERSHIP
-  membership: { className: 'family', label: 'MEMBERSHIP' },
-  vip: { className: 'vip', label: 'VIP' },
-  vvip: { className: 'vvip', label: 'VVIP' },
-};
+const MemberSummary = ({
+    tabChange,
+    profile,
+    availableMileage,
+    wishCount,
+    memberName,
+}) => {
+    const [couponCount, setCouponCount] = useState(0);
 
-const MemberSummary = ({ tabChange, profile, availablemileage, wishCount, memberName }) => {
-  const [couponCount, setCouponCount] = useState(0);
+    const grade = useMemo(
+        () => memberGrade[profile?.memberGradeName?.toLowerCase()],
+        [profile],
+    );
 
-  const grade = useMemo(() => memberGrade[profile?.memberGradeName?.toLowerCase()], [profile]);
+    useEffect(() => {
+        Promise.all([fetchCouponCount()]).catch(console.error);
+    }, []);
 
-  useEffect(() => {
-    Promise.all([fetchCouponCount()]).catch(console.error);
-  }, []);
+    async function fetchCouponCount() {
+        await syncCoupon();
+        const {
+            data: { usableCouponCnt },
+        } = await getCouponsSummary();
+        setCouponCount(usableCouponCnt);
+    }
 
-  async function fetchCouponCount() {
-    await syncCoupon();
-    const {
-      data: { usableCouponCnt },
-    } = await getCouponsSummary();
-    setCouponCount(usableCouponCnt);
-  }
-
-  return (
-    <div className="my_user">
-      <div className="user_profile">
-        <p className="user_name">
-          <span className="name">{memberName || ''}</span>님 안녕하세요 :)
-        </p>
-        <Link to="/my-page/member" className="user_modify under_line">
-          회원정보 수정
-        </Link>
-      </div>
-      <div className="user_info">
-        <ul>
-          <li className="user_item grade">
-            <Link to="/membership/benefit" className="user_tabs">
-              <span className="ico_txt">
-                <span className="txt_arrow">회원등급</span>
-              </span>
-              <span className="val_txt">
-                <span className={`val ${grade?.className}`}>{grade?.label || ''}</span>
-              </span>
-            </Link>
-          </li>
-          <li className="user_item mileage">
-            <a href="#mileage-tit" className="user_tabs" onClick={() => tabChange('mileage')}>
-              <span className="ico_txt">
-                <span className="txt_arrow">마일리지</span>
-              </span>
-              <span className="val_txt">
-                <span className="val">{toCurrencyString(availablemileage)}</span>M
-              </span>
-            </a>
-          </li>
-          <li className="user_item coupon">
-            <a href="#coupon-tit" className="user_tabs" onClick={() => tabChange('coupon')}>
-              <span className="ico_txt">
-                <span className="txt_arrow">쿠폰</span>
-              </span>
-              <span className="val_txt">
-                <span className="val">{couponCount}</span> 장
-              </span>
-            </a>
-          </li>
-          <li className="user_item like">
-            <a href="#wish-tit" className="user_tabs" onClick={() => tabChange('wish')}>
-              <span className="ico_txt">
-                <span className="txt_arrow">찜</span>
-              </span>
-              <span className="val_txt">
-                <span className="val">{wishCount}</span>
-              </span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+    return (
+        <div className='my_user'>
+            <div className='user_profile'>
+                <p className='user_name'>
+                    <span className='name'>{memberName || ''}</span>님
+                    안녕하세요 :)
+                </p>
+                <Link to='/my-page/member' className='user_modify under_line'>
+                    회원정보 수정
+                </Link>
+            </div>
+            <div className='user_info'>
+                <ul>
+                    <li className='user_item grade'>
+                        <Link to='/membership/benefit' className='user_tabs'>
+                            <span className='ico_txt'>
+                                <span className='txt_arrow'>회원등급</span>
+                            </span>
+                            <span className='val_txt'>
+                                <span className={`val ${grade?.className}`}>
+                                    {grade?.label || ''}
+                                </span>
+                            </span>
+                        </Link>
+                    </li>
+                    <li className='user_item mileage'>
+                        <a
+                            href='#mileage-tit'
+                            className='user_tabs'
+                            onClick={() => tabChange('mileage')}
+                        >
+                            <span className='ico_txt'>
+                                <span className='txt_arrow'>마일리지</span>
+                            </span>
+                            <span className='val_txt'>
+                                <span className='val'>
+                                    {toCurrencyString(availableMileage)}
+                                </span>
+                                M
+                            </span>
+                        </a>
+                    </li>
+                    <li className='user_item coupon'>
+                        <a
+                            href='#coupon-tit'
+                            className='user_tabs'
+                            onClick={() => tabChange('coupon')}
+                        >
+                            <span className='ico_txt'>
+                                <span className='txt_arrow'>쿠폰</span>
+                            </span>
+                            <span className='val_txt'>
+                                <span className='val'>{couponCount}</span> 장
+                            </span>
+                        </a>
+                    </li>
+                    <li className='user_item like'>
+                        <a
+                            href='#wish-tit'
+                            className='user_tabs'
+                            onClick={() => tabChange('wish')}
+                        >
+                            <span className='ico_txt'>
+                                <span className='txt_arrow'>찜</span>
+                            </span>
+                            <span className='val_txt'>
+                                <span className='val'>{wishCount}</span>
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default MemberSummary;
