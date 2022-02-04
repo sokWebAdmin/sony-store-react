5261698a35ac504d6675718aaae32cbbab085057
Jerome <jerome@geekstudio.co.kr>
2022-02-04 17:58:26 +0900

    fix: 주문 실패 페이지 버튼 CSS 수정 & 마침표 추가

--- src/pages/order/OrderFail.js
+++ src/pages/order/OrderFail.js
@@ -1,18 +1,12 @@
-import React, { useMemo } from 'react';
 import { Link } from 'react-router-dom';
+import qs from 'qs';
 
-//SEO
-import SEOHelmet from '../../components/SEOHelmet';
-
-//css
-import '../../assets/scss/contents.scss';
-import '../../assets/scss/order.scss';
-
-//
-import { getUrlParam } from '../../utils/location';
+import SEOHelmet from 'components/SEOHelmet';
+import 'assets/scss/contents.scss';
+import 'assets/scss/order.scss';
 
 const OrderFail = ({ location }) => {
-    const status = useMemo(() => getUrlParam('status'), [location]);
+    const query = qs.parse(location.search, { ignoreQueryPrefix: true });
 
     return (
         <>
@@ -31,7 +25,7 @@ const OrderFail = ({ location }) => {
                             <p className='order_confirm_box__dsc'>
                                 소니스토어를 이용해 주셔서 감사합니다!
                             </p>
-                            {status === 'PAY_FAIL' && (
+                            {query?.status === 'PAY_FAIL' && (
                                 <p
                                     className='order_confirm_box__dsc'
                                     style={{ fontWeight: 700 }}
@@ -44,7 +38,7 @@ const OrderFail = ({ location }) => {
                                     문의 하시기 바랍니다.
                                 </p>
                             )}
-                            {status === 'UNDEFINED' && (
+                            {query?.status === 'UNDEFINED' && (
                                 <p
                                     className='order_confirm_box__dsc'
                                     style={{ fontWeight: 700 }}
@@ -54,13 +48,13 @@ const OrderFail = ({ location }) => {
                                     다시 확인 바랍니다.
                                     <br />
                                     지속적으로 문제가 발생될 경우 고객센터에
-                                    문의 하시기 바랍니다
+                                    문의 하시기 바랍니다.
                                 </p>
                             )}
                             <div className='btn_box'>
                                 <Link
                                     to='/'
-                                    className='button button_negative'
+                                    className='button button_negative fail'
                                     type='button'
                                 >
                                     계속 쇼핑하기
