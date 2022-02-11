095a73022b8575d7fbdd9df1585b384f724ad93e
Jerome <jerome@geekstudio.co.kr>
2022-02-11 11:27:02 +0900

    refactor: Tab Component indent, code formatting 수정

--- src/components/search/Tab.js
+++ src/components/search/Tab.js
@@ -13,51 +13,57 @@ import { TAB_MAP } from '../../const/search';
 const tabs = Object.keys(TAB_MAP);
 
 export default function Tab({ tabState, setTabState, count, setReset }) {
-  const [swiperTab, setSwiperTab] = useState(null);
-  SwiperCore.use([Navigation]);
+    const [swiperTab, setSwiperTab] = useState(null);
+    SwiperCore.use([Navigation]);
 
-  return (
-    <>
-      <div className="swipe_tab swiper-container">
-        <div className="swiper-button-prev">
-          <a href="#" title="메뉴 더보기">
-            메뉴 더보기
-          </a>
-        </div>
-        <Swiper
-          className="swiper-wrapper"
-          navigation={{
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-          }}
-          tag={'ul'}
-          slidesPerView={'auto'}
-          freeMode={true}
-          onSwiper={setSwiperTab}
-        >
-          {tabs.map((tab, idx) => (
-            <SwiperSlide key={`${tab}${idx}`} className={`swiper-slide ${tabState === tab && 'active'}`} tag={'li'}>
-              <a
-                href={`#${tab}`}
-                onClick={(event) => {
-                  event.preventDefault();
-                  setTabState(tab);
-                  swiperTab?.slideTo(idx);
-                  setReset(true);
-                }}
-              >
-                {TAB_MAP[tab]}
-                {`(${count[tab]})`}
-              </a>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-        <div className="swiper-button-next">
-          <a href="#" title="메뉴 더보기">
-            메뉴 더보기
-          </a>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className='swipe_tab swiper-container'>
+                <div className='swiper-button-prev'>
+                    <a href='#' title='메뉴 더보기'>
+                        메뉴 더보기
+                    </a>
+                </div>
+                <Swiper
+                    className='swiper-wrapper'
+                    navigation={{
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
+                    }}
+                    tag={'ul'}
+                    slidesPerView={'auto'}
+                    freeMode={true}
+                    onSwiper={setSwiperTab}
+                >
+                    {tabs.map((tab, idx) => (
+                        <SwiperSlide
+                            key={`${tab}${idx}`}
+                            className={`swiper-slide ${
+                                tabState === tab && 'active'
+                            }`}
+                            tag={'li'}
+                        >
+                            <a
+                                href={`#${tab}`}
+                                onClick={(event) => {
+                                    event.preventDefault();
+                                    setTabState(tab);
+                                    swiperTab?.slideTo(idx);
+                                    setReset(true);
+                                }}
+                            >
+                                {TAB_MAP[tab]}
+                                {`(${count[tab]})`}
+                            </a>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+                <div className='swiper-button-next'>
+                    <a href='#' title='메뉴 더보기'>
+                        메뉴 더보기
+                    </a>
+                </div>
+            </div>
+        </>
+    );
 }
