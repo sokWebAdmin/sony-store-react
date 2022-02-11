241090e2d16e4baf3face3d6a3a1a672a353acb5
Jerome <jerome@geekstudio.co.kr>
2022-02-11 14:38:33 +0900

    chore: Newest Component에 PropTypes 추가

--- src/components/search/Newest.js
+++ src/components/search/Newest.js
@@ -1,40 +1,41 @@
-export default function Newest({ newest, setNewest }) {
-    return (
-        <div className='itemsort__drawer'>
-            <ul className='itemsort__items'>
-                <li
-                    className={`itemsort__item ${
-                        newest && 'itemsort__item--active'
-                    }`}
+import PropTypes from 'prop-types';
+
+const Newest = ({ newest, setNewest }) => (
+    <div className='itemsort__drawer'>
+        <ul className='itemsort__items'>
+            <li
+                className={`itemsort__item ${
+                    newest && 'itemsort__item--active'
+                }`}
+            >
+                <a
+                    href='#none'
+                    className='itemsort__item__link'
+                    onClick={() => setNewest(true)}
                 >
-                    <a
-                        href='#none'
-                        className='itemsort__item__link'
-                        onClick={(e) => {
-                            e.preventDefault();
-                            setNewest(true);
-                        }}
-                    >
-                        최신순
-                    </a>
-                </li>
-                <li
-                    className={`itemsort__item ${
-                        !newest && 'itemsort__item--active'
-                    }`}
+                    최신순
+                </a>
+            </li>
+            <li
+                className={`itemsort__item ${
+                    !newest && 'itemsort__item--active'
+                }`}
+            >
+                <a
+                    href='#none'
+                    className='itemsort__item__link'
+                    onClick={() => setNewest(false)}
                 >
-                    <a
-                        href='#none'
-                        className='itemsort__item__link'
-                        onClick={(e) => {
-                            e.preventDefault();
-                            setNewest(false);
-                        }}
-                    >
-                        오래된순
-                    </a>
-                </li>
-            </ul>
-        </div>
-    );
-}
+                    오래된순
+                </a>
+            </li>
+        </ul>
+    </div>
+);
+
+Newest.propTypes = {
+    newest: PropTypes.bool.isRequired,
+    setNewest: PropTypes.func.isRequired,
+};
+
+export default Newest;
