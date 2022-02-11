6e39dcea8b59c7c717f2e0f8a3bca3af6b1b4d76
Jerome <jerome@geekstudio.co.kr>
2022-02-11 14:32:56 +0900

    refactor: Newest Component indent, code formatting 수정

--- src/components/search/Newest.js
+++ src/components/search/Newest.js
@@ -1,32 +1,40 @@
 export default function Newest({ newest, setNewest }) {
-  return (
-    <div className="itemsort__drawer">
-      <ul className="itemsort__items">
-        <li className={`itemsort__item ${newest && 'itemsort__item--active'}`}>
-          <a
-            href="#none"
-            className="itemsort__item__link"
-            onClick={(e) => {
-              e.preventDefault();
-              setNewest(true);
-            }}
-          >
-            최신순
-          </a>
-        </li>
-        <li className={`itemsort__item ${!newest && 'itemsort__item--active'}`}>
-          <a
-            href="#none"
-            className="itemsort__item__link"
-            onClick={(e) => {
-              e.preventDefault();
-              setNewest(false);
-            }}
-          >
-            오래된순
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    return (
+        <div className='itemsort__drawer'>
+            <ul className='itemsort__items'>
+                <li
+                    className={`itemsort__item ${
+                        newest && 'itemsort__item--active'
+                    }`}
+                >
+                    <a
+                        href='#none'
+                        className='itemsort__item__link'
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setNewest(true);
+                        }}
+                    >
+                        최신순
+                    </a>
+                </li>
+                <li
+                    className={`itemsort__item ${
+                        !newest && 'itemsort__item--active'
+                    }`}
+                >
+                    <a
+                        href='#none'
+                        className='itemsort__item__link'
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setNewest(false);
+                        }}
+                    >
+                        오래된순
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
 }
