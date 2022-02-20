eac34f55d7ec4b70d6d9d0a65131c34c22323ef5
Jerome <jerome@geekstudio.co.kr>
2022-02-20 12:42:17 +0900

    fix: SEO Component 수정
     - canonical tag 추가

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,9 +1,9 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import PropTypes from 'prop-types';
 
-const SEO = ({ title, meta, og, twitter, itemprop }) => {
+const SEO = ({ title, canonical, meta, og, twitter, itemprop }) => {
     // TODO: 추후 별도 파일로 분리
-    const config = { title: '소니스토어', description: '' };
+    const config = { title: 'SonyStore', description: '' };
 
     return (
         <HelmetProvider>
@@ -11,65 +11,57 @@ const SEO = ({ title, meta, og, twitter, itemprop }) => {
                 {/* 타이틀  */}
                 <title>{title || config.title}</title>
 
+                {/* Canonical tag */}
+                {canonical && <link rel='canonical' href={canonical} />}
+
                 {/* 메타데이터 */}
-                {meta && (
-                    <>
-                        <meta
-                            name='title'
-                            content={meta.title || config.title}
-                        />
-                        <meta
-                            name='description'
-                            content={meta.description || config.description}
-                        />
-                    </>
+                {meta.title && (
+                    <meta name='title' content={meta.title || config.title} />
+                )}
+                {meta.description && (
+                    <meta
+                        name='description'
+                        content={meta.description || config.description}
+                    />
                 )}
 
                 {/* 오픈그래프 */}
-                {og && (
-                    <>
-                        <meta property='og.title' content={og.title || ''} />
-                        <meta property='og.type' content='website' />
-                        <meta
-                            property='og.description'
-                            content={og.description || ''}
-                        />
-                        <meta property='og.image' content={og.logo || ''} />
-                    </>
+                <meta property='og:type' content='website' />
+                {og.title && <meta property='og:title' content={og.title} />}
+                {og.description && (
+                    <meta property='og:description' content={og.description} />
                 )}
+                {og.image && <meta property='og:image' content={og.image} />}
 
                 {/* 트위터 */}
-                {twitter && (
-                    <>
-                        <meta
-                            property='twitter:card'
-                            content={twitter.card || ''}
-                        />
-                        <meta
-                            property='twitter:title'
-                            content={twitter.title || ''}
-                        />
-                        <meta
-                            property='twitter:description'
-                            content={twitter.description || ''}
-                        />
-                        <meta
-                            property='twitter:image'
-                            content={twitter.image || ''}
-                        />
-                    </>
+                {twitter.card && (
+                    <meta property='twitter:card' content={twitter.card} />
+                )}
+                {twitter.title && (
+                    <meta property='twitter:title' content={twitter.title} />
+                )}
+                {twitter.description && (
+                    <meta
+                        property='twitter:description'
+                        content={twitter.description}
+                    />
+                )}
+                {twitter.image && (
+                    <meta property='twitter:image' content={twitter.image} />
                 )}
 
                 {/* itemprop */}
-                {itemprop && (
-                    <>
-                        <meta itemprop='name' content={itemprop.name || ''} />
-                        <meta
-                            itemprop='description'
-                            content={itemprop.description || ''}
-                        />
-                        <meta itemprop='image' content={itemprop.image || ''} />
-                    </>
+                {itemprop.name && (
+                    <meta itemprop='name' content={itemprop.name || ''} />
+                )}
+                {itemprop.description && (
+                    <meta
+                        itemprop='description'
+                        content={itemprop.description || ''}
+                    />
+                )}
+                {itemprop.image && (
+                    <meta itemprop='image' content={itemprop.image || ''} />
                 )}
             </Helmet>
         </HelmetProvider>
@@ -78,6 +70,7 @@ const SEO = ({ title, meta, og, twitter, itemprop }) => {
 
 SEO.propTypes = {
     title: PropTypes.string.isRequired,
+    canonical: PropTypes.string,
     meta: PropTypes.shape({
         title: PropTypes.string,
         description: PropTypes.string,
@@ -86,11 +79,13 @@ SEO.propTypes = {
     og: PropTypes.shape({
         title: PropTypes.string,
         description: PropTypes.string,
+        image: PropTypes.string,
     }),
     twitter: PropTypes.shape({
         card: PropTypes.string,
         title: PropTypes.string,
         description: PropTypes.string,
+        image: PropTypes.string,
     }),
     itemprop: PropTypes.shape({
         name: PropTypes.string,
