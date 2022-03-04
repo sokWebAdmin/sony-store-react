20a637665b50b74fb6a71c8b74988aa5827527a5
sokWebAdmin <86636809+sokWebAdmin@users.noreply.github.com>
2022-03-04 17:38:38 +0900

    title 오탈자 수정
    title 오탈자 수정

--- src/const/seo.js
+++ src/const/seo.js
@@ -1,7 +1,7 @@
 const main = {
     canonical: 'https://store.sony.co.kr/',
     meta: {
-        tittle: '소니스토어',
+        title: '소니스토어',
         description:
             '소니 공식 온라인 스토어에서 소니의 최신 제품을 만나고, 카메라, 렌즈, 헤드폰, 이어폰을 구입하는 것은 물론, 소니의 액세서리 및 기기 지원에 대해 살펴보세요.',
     },
@@ -29,7 +29,7 @@ const main = {
 const storeInfo = {
     canonical: 'https://store.sony.co.kr/store-info',
     meta: {
-        tittle: '소니스토어 직영점 안내',
+        title: '소니스토어 직영점 안내',
         description: '소니스토어 압구정점 위치 및 운영시간 안내',
     },
     og: {
@@ -53,7 +53,7 @@ const storeInfo = {
 const benefit = {
     canonical: 'https://store.sony.co.kr/membership/benefit',
     meta: {
-        tittle: '소니스토어 회원을 위한 멤버십 서비스',
+        title: '소니스토어 회원을 위한 멤버십 서비스',
         description:
             '소니스토어 회원이 되시면 다양한 멤버십 혜택을 누리실 수 있습니다!',
     },
@@ -81,7 +81,7 @@ const benefit = {
 const videoCourse = {
     canonical: 'https://store.sony.co.kr/video-course',
     meta: {
-        tittle: '“소니 제품 동영상 강좌',
+        title: '“소니 제품 동영상 강좌',
         description:
             '제품의 기본 기능부터 숨겨진 기능까지 한번에 알 수 있는 동영상 강좌',
     },
