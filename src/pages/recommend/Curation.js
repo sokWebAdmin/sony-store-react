ef834cd8814aebf9dafd2c888a37a27106367092
Jerome <jerome@geekstudio.co.kr>
2022-03-05 13:16:38 +0900

    refactor: 선물제안 페이지 indent, code formatting 수정

--- src/pages/recommend/Curation.js
+++ src/pages/recommend/Curation.js
@@ -8,7 +8,12 @@ import '../../assets/scss/recommend.scss';
 
 //lib
 import { Controller } from 'react-scrollmagic';
-import SwiperCore, { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper/core';
+import SwiperCore, {
+    Navigation,
+    Pagination,
+    Scrollbar,
+    Autoplay,
+} from 'swiper/core';
 
 //lib-css
 import 'swiper/components/navigation/navigation.scss';
@@ -20,41 +25,45 @@ import SEOHelmet from '../../components/SEOHelmet';
 import { bannerCode } from '../../bannerCode';
 
 export default function Curation() {
-  const [url, setUrl] = useState('');
-  const { curation } = bannerCode;
-  const getEvent = useCallback(async () => {
-    const response = await getEventByEventNo(curation);
-    if (response.status === 400) return;
-    setUrl(response.data.url);
-  }, []);
+    const [url, setUrl] = useState('');
+    const { curation } = bannerCode;
+    const getEvent = useCallback(async () => {
+        const response = await getEventByEventNo(curation);
+        if (response.status === 400) return;
+        setUrl(response.data.url);
+    }, []);
 
-  const iframePart = () => {
-    return {
-      __html: `<iframe src="${url}" width="100%" height="5467px"></iframe>`,
+    const iframePart = () => {
+        return {
+            __html: `<iframe src="${url}" width="100%" height="5467px"></iframe>`,
+        };
     };
-  };
 
-  useEffect(() => {
-    getEvent();
-  }, [getEvent]);
+    useEffect(() => {
+        getEvent();
+    }, [getEvent]);
 
-  SwiperCore.use([Navigation, Pagination, Scrollbar, Autoplay]);
+    SwiperCore.use([Navigation, Pagination, Scrollbar, Autoplay]);
 
-  return (
-    <>
-      <SEOHelmet title={'스토어 추천제품 : 선물제안'} />
-      <Controller>
-        <div className="contents recommend">
-          <div className="container">
-            <div className="content">
-              <div
-                style={{ width: '100%', height: '100%', textAlign: 'center' }}
-                dangerouslySetInnerHTML={iframePart()}
-              />
-            </div>
-          </div>
-        </div>
-      </Controller>
-    </>
-  );
+    return (
+        <>
+            <SEOHelmet title={'스토어 추천제품 : 선물제안'} />
+            <Controller>
+                <div className='contents recommend'>
+                    <div className='container'>
+                        <div className='content'>
+                            <div
+                                style={{
+                                    width: '100%',
+                                    height: '100%',
+                                    textAlign: 'center',
+                                }}
+                                dangerouslySetInnerHTML={iframePart()}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </Controller>
+        </>
+    );
 }
