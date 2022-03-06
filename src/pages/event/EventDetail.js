54411d542c3f8c723c05d3f70f9230805f78dc6e
Jerome <jerome@geekstudio.co.kr>
2022-03-06 23:15:29 +0900

    refactor: 기획전 상세페이지 indent, code formatting 수정

--- src/pages/event/EventDetail.js
+++ src/pages/event/EventDetail.js
@@ -6,42 +6,61 @@ import SEOHelmet from '../../components/SEOHelmet';
 import EventProducts from '../../components/event/EventProducts';
 
 const EventDetail = () => {
-  const { eventNo } = useParams();
-  const onlyMo = useMediaQuery('(max-width: 640px)');
-  const [event, setEvent] = useState(null);
+    const { eventNo } = useParams();
+    const onlyMo = useMediaQuery('(max-width: 640px)');
+    const [event, setEvent] = useState(null);
 
-  const fetchDetailEvent = async () => {
-    const response = await getEventByEventNo(eventNo, { soldout: true });
-    setEvent(response.data);
-  };
+    const fetchDetailEvent = async () => {
+        const response = await getEventByEventNo(eventNo, { soldout: true });
+        setEvent(response.data);
+    };
 
-  useEffect(() => {
-    fetchDetailEvent();
-  }, []);
+    useEffect(() => {
+        fetchDetailEvent();
+    }, []);
 
-  const TopContent = ({ url, type }) => {
+    const TopContent = ({ url, type }) => {
+        return (
+            <>
+                {type === 'FILE' ? (
+                    <img src={url} alt='' style={{ width: '100%' }} />
+                ) : (
+                    <div dangerouslySetInnerHTML={{ __html: url }} />
+                )}
+            </>
+        );
+    };
+
     return (
-      <>
-        {type === 'FILE' ? <img src={url} alt="" style={{ width: '100%' }} /> :
-          <div dangerouslySetInnerHTML={{ __html: url }} />}
-      </>
+        <>
+            <SEOHelmet title={event?.label} />
+            {event && (
+                <div className='contents events'>
+                    <div className='container full'>
+                        <div className='content employee'>
+                            <TopContent
+                                url={
+                                    onlyMo
+                                        ? event.top.mobile.url
+                                        : event.top.pc.url
+                                }
+                                type={
+                                    onlyMo
+                                        ? event.top.mobile.type
+                                        : event.top.pc.type
+                                }
+                            />
+                            <EventProducts
+                                event={event}
+                                gift={true}
+                                sectionImage={true}
+                            />
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
     );
-  };
-
-  return (
-    <>
-      <SEOHelmet title={event?.label} />
-      {event && <div className="contents events">
-        <div className="container full">
-          <div className="content employee">
-            <TopContent url={onlyMo ? event.top.mobile.url : event.top.pc.url}
-                        type={onlyMo ? event.top.mobile.type : event.top.pc.type} />
-            <EventProducts event={event} gift={true} sectionImage={true} />
-          </div>
-        </div>
-      </div>}
-    </>
-  );
 };
 
 export default EventDetail;
