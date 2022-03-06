a2e4b3344d9c88a210a04d11655b9b67ecd8b367
Jerome <jerome@geekstudio.co.kr>
2022-03-06 20:39:24 +0900

    refactor: 상품 관련 페이지 indent, code formatting 수정

--- src/pages/products/Category.js
+++ src/pages/products/Category.js
@@ -9,50 +9,62 @@ import '../../assets/scss/category.scss';
 //utils
 import CategoryHeader from '../../components/products/CategoryHeader';
 import ProductList from '../../components/products/ProductList';
-import { getCategoryByKey, useCategoryState, useGetCategoryByKey } from '../../context/category.context';
+import {
+    getCategoryByKey,
+    useCategoryState,
+    useGetCategoryByKey,
+} from '../../context/category.context';
 
 export default function Category({ match, history }) {
-  const category = useGetCategoryByKey('url', match.url);
+    const category = useGetCategoryByKey('url', match.url);
 
-  const { categories } = useCategoryState();
+    const { categories } = useCategoryState();
 
-  if (categories.length > 0 && !category) {
-    history.replace('/404');
-  }
-
-  const [currentCategory, setCurrentCategory] = useState(null);
-
-  if (category && !currentCategory) {
-    setCurrentCategory(category);
-  }
-
-  const changeCurrentCategoryByNo = (categoryNo) => {
-    const tempCategory = getCategoryByKey([category], 'categoryNo', categoryNo);
-
-    if (tempCategory) {
-      setCurrentCategory(tempCategory);
+    if (categories.length > 0 && !category) {
+        history.replace('/404');
     }
-  };
 
-  return (
-    <>
-      <SEOHelmet title={`제품목록:${category?.label || ''}`} />
+    const [currentCategory, setCurrentCategory] = useState(null);
 
-      <div className="category">
-        <div className="container">
-          <div className="content">
-            {category && (
-              <>
-                <CategoryHeader
-                  category={category}
-                  changeCurrentCategoryByNo={changeCurrentCategoryByNo}
-                ></CategoryHeader>
-                <ProductList category={currentCategory}></ProductList>
-              </>
-            )}
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    if (category && !currentCategory) {
+        setCurrentCategory(category);
+    }
+
+    const changeCurrentCategoryByNo = (categoryNo) => {
+        const tempCategory = getCategoryByKey(
+            [category],
+            'categoryNo',
+            categoryNo,
+        );
+
+        if (tempCategory) {
+            setCurrentCategory(tempCategory);
+        }
+    };
+
+    return (
+        <>
+            <SEOHelmet title={`제품목록:${category?.label || ''}`} />
+
+            <div className='category'>
+                <div className='container'>
+                    <div className='content'>
+                        {category && (
+                            <>
+                                <CategoryHeader
+                                    category={category}
+                                    changeCurrentCategoryByNo={
+                                        changeCurrentCategoryByNo
+                                    }
+                                ></CategoryHeader>
+                                <ProductList
+                                    category={currentCategory}
+                                ></ProductList>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
