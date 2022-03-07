dd08c71bd5bce30c8f940535a23ab544cd2cceb8
Jerome <jerome@geekstudio.co.kr>
2022-03-07 09:46:08 +0900

    feat: 기획전 상세 페이지에 SEO 데이터 추가

--- src/pages/event/EventDetail.js
+++ src/pages/event/EventDetail.js
@@ -1,23 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { getEventByEventNo } from '../../api/display';
-import { useMediaQuery } from '../../hooks';
-import SEOHelmet from '../../components/SEOHelmet';
-import EventProducts from '../../components/event/EventProducts';
+import { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+
+import SEO from 'components/SEO';
+import EventProducts from 'components/event/EventProducts';
+import { getEventByEventNo } from 'api/display';
+import { useMediaQuery } from 'hooks';
 
 const EventDetail = () => {
     const { eventNo } = useParams();
     const onlyMo = useMediaQuery('(max-width: 640px)');
     const [event, setEvent] = useState(null);
-
-    const fetchDetailEvent = async () => {
-        const response = await getEventByEventNo(eventNo, { soldout: true });
-        setEvent(response.data);
-    };
+    const [seo, setSeo] = useState();
+    const history = useHistory();
 
     useEffect(() => {
-        fetchDetailEvent();
-    }, []);
+        (async () => {
+            try {
+                const { data } = await getEventByEventNo(eventNo, {
+                    soldout: true,
+                });
+
+                setEvent(data);
+                setSeo((prev) => ({
+                    ...prev,
+                    meta: {
+                        title: data.label,
+                        description: data.promotionText,
+                    },
+                    og: {
+                        title: data.label,
+                        description: data.promotionText,
+                        image: data.pcImageUrl,
+                    },
+                    twitter: {
+                        card: 'summary',
+                        title: data.label,
+                        description: data.promotionText,
+                        image: data.pcImageUrl,
+                    },
+                    itemprop: {
+                        name: data.label,
+                        description: data.promotionText,
+                        image: data.pcImageUrl,
+                    },
+                }));
+            } catch (error) {
+                history.push('/404');
+            }
+        })();
+    }, [eventNo, history]);
 
     const TopContent = ({ url, type }) => {
         return (
@@ -33,7 +64,7 @@ const EventDetail = () => {
 
     return (
         <>
-            <SEOHelmet title={event?.label} />
+            {seo && <SEO data={seo} />}
             {event && (
                 <div className='contents events'>
                     <div className='container full'>
