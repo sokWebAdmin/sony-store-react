1339ad21429583daa6c68416afead8aa4412d38f
Jerome <jerome@geekstudio.co.kr>
2022-03-07 09:54:57 +0900

    refactor: 404페이지 indent, code formatting 수정

--- src/pages/error/error404.js
+++ src/pages/error/error404.js
@@ -1,41 +1,58 @@
- import { React } from 'react';
+import { React } from 'react';
 
- //SEO
- import SEOHelmet from '../../components/SEOHelmet';
- 
- //api
+//SEO
+import SEOHelmet from '../../components/SEOHelmet';
 
- //css
- import "../../assets/scss/contents.scss"
- 
- //img
- import errorLogo from '../../assets/images/common/error_logo.svg';
- import ic404 from '../../assets/images/common/ic_404.svg';
+//api
 
- //utils
-import { useHistory } from "react-router-dom";
+//css
+import '../../assets/scss/contents.scss';
 
- export default function Error404() {
+//img
+import errorLogo from '../../assets/images/common/error_logo.svg';
+import ic404 from '../../assets/images/common/ic_404.svg';
+
+//utils
+import { useHistory } from 'react-router-dom';
+
+export default function Error404() {
     const history = useHistory();
 
-     return (
-         <>
-         <SEOHelmet title={"페이지를 찾을 수 없습니다"} />
+    return (
+        <>
+            <SEOHelmet title={'페이지를 찾을 수 없습니다'} />
 
-         <div className="error">
-            <div className="error__container">
-                <div className="error_content">
-                <i className="icon"><img src={ic404} alt="404 Error" /></i>
-                <h2 className="title">요청하신 페이지를 <span>표시할 수 없습니다.</span></h2>
-                <p className="eng">The page that you are looking for is unavailable.</p>
-                <p className="desc">오래된 URL이거나 폐지된 페이지일 수 있으니,<br />
-                    메인(홈)으로 이동하셔서 원하시는 정보를 찾아주십시오. 감사합니다.</p>
-                <div className="btn_box">
-                    <a  onClick={()=>{history.push('/')}} className="btn btn_dark">홈으로</a>
-                </div>
+            <div className='error'>
+                <div className='error__container'>
+                    <div className='error_content'>
+                        <i className='icon'>
+                            <img src={ic404} alt='404 Error' />
+                        </i>
+                        <h2 className='title'>
+                            요청하신 페이지를 <span>표시할 수 없습니다.</span>
+                        </h2>
+                        <p className='eng'>
+                            The page that you are looking for is unavailable.
+                        </p>
+                        <p className='desc'>
+                            오래된 URL이거나 폐지된 페이지일 수 있으니,
+                            <br />
+                            메인(홈)으로 이동하셔서 원하시는 정보를
+                            찾아주십시오. 감사합니다.
+                        </p>
+                        <div className='btn_box'>
+                            <a
+                                onClick={() => {
+                                    history.push('/');
+                                }}
+                                className='btn btn_dark'
+                            >
+                                홈으로
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
-            </div>
-         </>
-     );
- }
+        </>
+    );
+}
