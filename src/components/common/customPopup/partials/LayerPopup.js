c6a3650ca9c4d282afa22f76a5f00ecf750abfff
Jerome <jerome@geekstudio.co.kr>
2022-03-13 14:27:09 +0900

    refactor: LayerPopup Component indent, code formatting 수정

--- src/components/common/customPopup/partials/LayerPopup.js
+++ src/components/common/customPopup/partials/LayerPopup.js
@@ -5,57 +5,80 @@ import '../../../../assets/scss/partials/customPopup.scss';
 const Z_INDEX_START_AT = 990;
 
 const LayerPopup = ({ popup, todayNotShow, index }) => {
-  const [show, setShow] = useState(true);
+    const [show, setShow] = useState(true);
 
-  const contentArea = useRef();
+    const contentArea = useRef();
 
-  const changed = () => {
-    setContent(popup.content);
-  };
+    const changed = () => {
+        setContent(popup.content);
+    };
 
-  useEffect(() => changed(), [popup]);
+    useEffect(() => changed(), [popup]);
 
-  const sizeStyle = useMemo(() => ({
-    width: popup.width + 'px',
-    height: popup.height + 'px',
-  }), [popup]);
+    const sizeStyle = useMemo(
+        () => ({
+            width: popup.width + 'px',
+            height: popup.height + 'px',
+        }),
+        [popup],
+    );
 
-  function setContent (stringifyHTML) {
-    if (show) {
-      contentArea.current.innerHTML = stringifyHTML;
+    function setContent(stringifyHTML) {
+        if (show) {
+            contentArea.current.innerHTML = stringifyHTML;
+        }
     }
-  }
 
-  return (
-    show && (
-      <>
-        <div className="layer_mask" tabIndex="0"
-             style={{ display: 'block', zIndex: Z_INDEX_START_AT + index }} />
-        <div className='popup_wrap custom_popup_layer'
-             style={{ display: 'block' }}>
-          <div className="pop_inner">
-            <div className='pop_cont custom_popup_layer--container'
-                 ref={contentArea} style={sizeStyle} />
-            <button type="button"
-                    className="ico_x closed delete custom_popup_layer--delete"
-                    title="팝업창 닫기"
-                    onClick={() => setShow(false)}
-            >
-              <span className="delete">팝업창 닫기</span>
-            </button>
-            <div className="today_not_opened check">
-              <input type="checkbox" className="inp_check"
-                     id={popup.popupNo + '_close'} />
-              <label onClick={() => {
-                todayNotShow.set(popup.popupNo);
-                setShow(false);
-              }}
-                     htmlFor={popup.popupNo + '_close'}>오늘하루 그만보기</label>
-            </div>
-          </div>
-        </div>
-      </>)
-  );
+    return (
+        show && (
+            <>
+                <div
+                    className='layer_mask'
+                    tabIndex='0'
+                    style={{
+                        display: 'block',
+                        zIndex: Z_INDEX_START_AT + index,
+                    }}
+                />
+                <div
+                    className='popup_wrap custom_popup_layer'
+                    style={{ display: 'block' }}
+                >
+                    <div className='pop_inner'>
+                        <div
+                            className='pop_cont custom_popup_layer--container'
+                            ref={contentArea}
+                            style={sizeStyle}
+                        />
+                        <button
+                            type='button'
+                            className='ico_x closed delete custom_popup_layer--delete'
+                            title='팝업창 닫기'
+                            onClick={() => setShow(false)}
+                        >
+                            <span className='delete'>팝업창 닫기</span>
+                        </button>
+                        <div className='today_not_opened check'>
+                            <input
+                                type='checkbox'
+                                className='inp_check'
+                                id={popup.popupNo + '_close'}
+                            />
+                            <label
+                                onClick={() => {
+                                    todayNotShow.set(popup.popupNo);
+                                    setShow(false);
+                                }}
+                                htmlFor={popup.popupNo + '_close'}
+                            >
+                                오늘하루 그만보기
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    );
 };
 
 export default LayerPopup;
