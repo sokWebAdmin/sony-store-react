816f65c22fa8bc0b573930ee410e4c9b08421991
Jerome <jerome@geekstudio.co.kr>
2022-03-20 11:40:18 +0900

    refactor: ProductSearch Component indent, code formatting 수정

--- src/components/search/ProductResult.js
+++ src/components/search/ProductResult.js
@@ -6,110 +6,145 @@ import _ from 'lodash';
 import { postProductsGroupManagementCode } from '../../api/product';
 import ViewMore from '../common/ViewMore';
 
-export default function ProductResult({ productList, orderBy, setOrderBy, productCount, searchProduct, keyword }) {
-  const [mobileOrderOpen, setMobileOrderOpen] = useState(false);
-  const [products, setProducts] = useState(productList);
+export default function ProductResult({
+    productList,
+    orderBy,
+    setOrderBy,
+    productCount,
+    searchProduct,
+    keyword,
+}) {
+    const [mobileOrderOpen, setMobileOrderOpen] = useState(false);
+    const [products, setProducts] = useState(productList);
 
-  const fetchGroupManagementMappingProducts = async (groupManagementCodes, productList) => {
-    const { data } = await postProductsGroupManagementCode({
-      groupManagementCodes,
-      saleStatus: 'ALL_CONDITIONS',
-      isSoldOut: true,
-    });
+    const fetchGroupManagementMappingProducts = async (
+        groupManagementCodes,
+        productList,
+    ) => {
+        const { data } = await postProductsGroupManagementCode({
+            groupManagementCodes,
+            saleStatus: 'ALL_CONDITIONS',
+            isSoldOut: true,
+        });
 
-    const groupByCode = _.chain(data)
-      .map(({ groupManagementMappingProducts, groupManagementCode }) => ({
-        groupManagementCode,
-        groupManagementMappingProducts,
-      }))
-      .groupBy('groupManagementCode')
-      .value();
+        const groupByCode = _.chain(data)
+            .map(({ groupManagementMappingProducts, groupManagementCode }) => ({
+                groupManagementCode,
+                groupManagementMappingProducts,
+            }))
+            .groupBy('groupManagementCode')
+            .value();
 
-    setProducts(() => {
-      let products = _.map(productList, (p) => ({
-        ...p,
-        groupManagementMappingProducts: _.head(groupByCode[p.groupManagementCode])?.groupManagementMappingProducts,
-      }));
-      if (orderBy === 'OLD_PRODUCT') {
-        products.reverse();
-      }
-      return products;
-    });
-  };
+        setProducts(() => {
+            let products = _.map(productList, (p) => ({
+                ...p,
+                groupManagementMappingProducts: _.head(
+                    groupByCode[p.groupManagementCode],
+                )?.groupManagementMappingProducts,
+            }));
+            if (orderBy === 'OLD_PRODUCT') {
+                products.reverse();
+            }
+            return products;
+        });
+    };
 
-  const getOrderByLabel = () => {
-    switch (orderBy) {
-      case 'RECENT_PRODUCT':
-        return '최신순';
-      case 'LOW_PRICE':
-        return '낮은 가격순';
-      case 'DISCOUNTED_PRICE':
-        return '높은 가격순';
-      case 'OLD_PRODUCT':
-        return '오래된 순';
-      default:
-        throw new Error('Unknown orderBy type');
-    }
-  };
+    const getOrderByLabel = () => {
+        switch (orderBy) {
+            case 'RECENT_PRODUCT':
+                return '최신순';
+            case 'LOW_PRICE':
+                return '낮은 가격순';
+            case 'DISCOUNTED_PRICE':
+                return '높은 가격순';
+            case 'OLD_PRODUCT':
+                return '오래된 순';
+            default:
+                throw new Error('Unknown orderBy type');
+        }
+    };
 
-  const codes = _.chain(productList)
-    .flatMap(({ groupManagementCode }) => groupManagementCode)
-    .compact()
-    .uniq()
-    .value();
+    const codes = _.chain(productList)
+        .flatMap(({ groupManagementCode }) => groupManagementCode)
+        .compact()
+        .uniq()
+        .value();
 
-  useEffect(() => fetchGroupManagementMappingProducts(codes, productList), [productList]);
+    useEffect(
+        () => fetchGroupManagementMappingProducts(codes, productList),
+        [productList],
+    );
 
-  return (
-    <>
-      <div className="section_top">
-        <h2 className="title">
-          제품<span>({productCount})</span>
-        </h2>
-        <div className={`itemsort ${mobileOrderOpen ? 'itemsort--open' : ''}`} aria-label="상품 정렬">
-          <button
-            className="itemsort__button"
-            onClick={() => {
-              setMobileOrderOpen(!mobileOrderOpen);
-            }}
-          >
-            <span className="itemsort__button__label sr-only">정렬기준:</span>
-            <span className="itemsort__button__selected">{getOrderByLabel()}</span>
-          </button>
-          <div className="itemsort__drawer">
-            <ul className="itemsort__items">
-              {orderList.map((order, idx) => (
-                <li
-                  key={`${order.label}${idx}`}
-                  className={`itemsort__item ${orderBy === order.orderBy ? 'itemsort__item--active' : ''}`}
+    return (
+        <>
+            <div className='section_top'>
+                <h2 className='title'>
+                    제품<span>({productCount})</span>
+                </h2>
+                <div
+                    className={`itemsort ${
+                        mobileOrderOpen ? 'itemsort--open' : ''
+                    }`}
+                    aria-label='상품 정렬'
                 >
-                  <a
-                    href={`#${order.orderBy}`}
-                    className="itemsort__item__link"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      setOrderBy(order.orderBy);
-                      setMobileOrderOpen(!mobileOrderOpen);
-                    }}
-                  >
-                    {order.label}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="product__list product__list--lite">
-        {products && products.map((item, itemIndex) => <Product key={itemIndex} product={item} />)}
-      </div>
-      {productCount >= 9 && (
-        <ViewMore
-          totalCount={productCount}
-          viewMore={(pageNumber) => searchProduct(keyword, orderBy, pageNumber)}
-          pageSize={PAGE_SIZE.PRODUCT}
-        />
-      )}
-    </>
-  );
+                    <button
+                        className='itemsort__button'
+                        onClick={() => {
+                            setMobileOrderOpen(!mobileOrderOpen);
+                        }}
+                    >
+                        <span className='itemsort__button__label sr-only'>
+                            정렬기준:
+                        </span>
+                        <span className='itemsort__button__selected'>
+                            {getOrderByLabel()}
+                        </span>
+                    </button>
+                    <div className='itemsort__drawer'>
+                        <ul className='itemsort__items'>
+                            {orderList.map((order, idx) => (
+                                <li
+                                    key={`${order.label}${idx}`}
+                                    className={`itemsort__item ${
+                                        orderBy === order.orderBy
+                                            ? 'itemsort__item--active'
+                                            : ''
+                                    }`}
+                                >
+                                    <a
+                                        href={`#${order.orderBy}`}
+                                        className='itemsort__item__link'
+                                        onClick={(e) => {
+                                            e.preventDefault();
+                                            setOrderBy(order.orderBy);
+                                            setMobileOrderOpen(
+                                                !mobileOrderOpen,
+                                            );
+                                        }}
+                                    >
+                                        {order.label}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className='product__list product__list--lite'>
+                {products &&
+                    products.map((item, itemIndex) => (
+                        <Product key={itemIndex} product={item} />
+                    ))}
+            </div>
+            {productCount >= 9 && (
+                <ViewMore
+                    totalCount={productCount}
+                    viewMore={(pageNumber) =>
+                        searchProduct(keyword, orderBy, pageNumber)
+                    }
+                    pageSize={PAGE_SIZE.PRODUCT}
+                />
+            )}
+        </>
+    );
 }
