2781a920de8b1b284c5acc5ec7fc58dd407f89da
Jerome <jerome@geekstudio.co.kr>
2022-03-24 00:16:39 +0900

    refactor: RelatedProducts Component indent, code formatting 수정

--- src/components/products/RelatedProducts.js
+++ src/components/products/RelatedProducts.js
@@ -1,47 +1,58 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Navigation, Pagination, Scrollbar, Autoplay, Controller } from 'swiper/core';
+import SwiperCore, {
+    Navigation,
+    Pagination,
+    Scrollbar,
+    Autoplay,
+    Controller,
+} from 'swiper/core';
 import Product from './Product';
 import { useCategoryState } from '../../context/category.context';
 
 export default function RelatedProducts({ products, reset }) {
-  SwiperCore.use([Navigation, Pagination, Scrollbar, Autoplay, Controller]);
-  const { categories } = useCategoryState();
+    SwiperCore.use([Navigation, Pagination, Scrollbar, Autoplay, Controller]);
+    const { categories } = useCategoryState();
 
-  return (
-    <div className="product_cont first recommend">
-      <div className="slide_box together_prd_slider swiper-container">
-        <h2 className="title">함께 구매하시면 좋은 추천 제품</h2>
-        <Swiper
-          className="swiper-wrapper product_List"
-          slidesPerView={2}
-          spaceBetween={10}
-          navigation={{
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-          }}
-          breakpointsinverse={'true'}
-          breakpoints={{
-            768: {
-              slidesPerView: 3,
-              spaceBetween: 10,
-            },
-            1200: {
-              slidesPerView: 4,
-              spaceBetween: 24,
-            },
-          }}
-        >
-          <>
-            {products.map((product) => (
-              <SwiperSlide key={product.productNo}>
-                <Product product={product} categories={categories} reset={reset} micro={true} />
-              </SwiperSlide>
-            ))}
-          </>
-        </Swiper>
-        <div className="swiper-button-next">다음</div>
-        <div className="swiper-button-prev">이전</div>
-      </div>
-    </div>
-  );
+    return (
+        <div className='product_cont first recommend'>
+            <div className='slide_box together_prd_slider swiper-container'>
+                <h2 className='title'>함께 구매하시면 좋은 추천 제품</h2>
+                <Swiper
+                    className='swiper-wrapper product_List'
+                    slidesPerView={2}
+                    spaceBetween={10}
+                    navigation={{
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
+                    }}
+                    breakpointsinverse={'true'}
+                    breakpoints={{
+                        768: {
+                            slidesPerView: 3,
+                            spaceBetween: 10,
+                        },
+                        1200: {
+                            slidesPerView: 4,
+                            spaceBetween: 24,
+                        },
+                    }}
+                >
+                    <>
+                        {products.map((product) => (
+                            <SwiperSlide key={product.productNo}>
+                                <Product
+                                    product={product}
+                                    categories={categories}
+                                    reset={reset}
+                                    micro={true}
+                                />
+                            </SwiperSlide>
+                        ))}
+                    </>
+                </Swiper>
+                <div className='swiper-button-next'>다음</div>
+                <div className='swiper-button-prev'>이전</div>
+            </div>
+        </div>
+    );
 }
