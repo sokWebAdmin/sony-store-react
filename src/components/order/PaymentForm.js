e6fff6b95d9b4fe9728aa33c88b338084950f3ed
ykawa22 <ykawa2@ipartners.co.kr>
2022-04-20 17:52:21 +0900

    소비자 피해보상보험 서비스 영역 추가

--- src/components/order/PaymentForm.js
+++ src/components/order/PaymentForm.js
@@ -102,6 +102,169 @@ const PaymentForm = ({ payment, setPayment, orderSheetNo }) => {
                       </li>
                     </ul>
                   </div>
+                  <div className="sgic_box">
+                    <strong className="sgic_box_tit">소비자 피해보상보험 서비스</strong>
+                    <div className="sgic_box_radio acc_radio">
+                      <div className="radio_box">
+                        <input type="radio" className="inp_radio" id="sgic_tab_radio1" name="sgictabradio" />
+                        <label htmlFor="sgic_tab_radio1" className="contentType">신청함</label>
+                      </div>
+                      <div className="radio_box">
+                        <input type="radio" className="inp_radio" id="sgic_tab_radio2" name="sgictabradio" defaultChecked="checked" />
+                        <label htmlFor="sgic_tab_radio2" className="contentType">신청 안함</label>
+                      </div>
+                    </div>
+                    <div className="sgic_box_result_cont sgic_tab_radio1">
+                      <div className="acc_form">
+                        <div className="acc_cell vat">
+                          <label htmlFor="sgic_birth">생년월일</label>
+                        </div>
+                        <div className="acc_cell parent">
+                          <div className="select_ui_zone btm_line">
+                            <a href="#" className="selected_btn" data-default-text="선택">선택</a>
+                            <div className="select_inner" style={{display: 'none'}}>
+                              <ul className="select_opt">
+                                <li>
+                                  <a href="#" className="opt_list">
+                                    <div className="item">1990</div>
+                                  </a>
+                                </li>
+                                <li>
+                                  <a href="#" className="opt_list">
+                                    <div className="item">1991</div>
+                                  </a>
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+                          <span className="sgic_birth_txt">년</span>
+                          <div className="select_ui_zone btm_line">
+                            <a href="#" className="selected_btn" data-default-text="선택">선택</a>
+                            <div className="select_inner" style={{display: 'none'}}>
+                              <ul className="select_opt">
+                                <li>
+                                  <a href="#" className="opt_list">
+                                    <div className="item">01</div>
+                                  </a>
+                                </li>
+                                <li>
+                                  <a href="#" className="opt_list">
+                                    <div className="item">02</div>
+                                  </a>
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+                          <span className="sgic_birth_txt">월</span>
+                          <div className="select_ui_zone btm_line">
+                            <a href="#" className="selected_btn" data-default-text="선택">선택</a>
+                            <div className="select_inner" style={{display: 'none'}}>
+                              <ul className="select_opt">
+                                <li>
+                                  <a href="#" className="opt_list">
+                                    <div className="item">01</div>
+                                  </a>
+                                </li>
+                                <li>
+                                  <a href="#" className="opt_list">
+                                    <div className="item">02</div>
+                                  </a>
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+                          <span className="sgic_birth_txt">일</span>
+                        </div>
+                      </div>
+                      <div className="acc_form">
+                        <div className="acc_cell vat">
+                          <label htmlFor="sgic_gender_radio1">성별</label>
+                        </div>
+                        <div className="acc_cell parent">
+                          <div className="acc_radio">
+                            <div className="radio_box">
+                              <input type="radio" className="inp_radio" id="sgic_gender_radio1" name="sgicgenderradio" />
+                              <label htmlFor="sgic_gender_radio1" className="contentType">남자</label>
+                            </div>
+                            <div className="radio_box">
+                              <input type="radio" className="inp_radio" id="sgic_gender_radio2" name="sgicgenderradio" />
+                              <label htmlFor="sgic_gender_radio2" className="contentType">여자</label>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="acc_form">
+                        <div className="acc_cell vat">
+                          <label htmlFor="sgic_email">이메일</label>
+                        </div>
+                        <div className="acc_cell parent">
+                          <div className="acc_inp type3">
+                            <input type="text" className="inp" id="sgic_email" placeholder="(예 : sony@sony.co.kr)" />
+                            <span className="focus_bg" />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="acc_form">
+                        <div className="acc_cell vat">
+                          <label htmlFor="sgic_agree_radio1">개인정보 이용동의</label>
+                        </div>
+                        <div className="acc_cell parent">
+                          <div className="acc_radio">
+                            <div className="radio_box">
+                              <input type="radio" className="inp_radio" id="sgic_agree_radio1" name="sgicagree" />
+                              <label htmlFor="sgic_agree_radio1" className="contentType">동의함</label>
+                            </div>
+                            <div className="radio_box">
+                              <input type="radio" className="inp_radio" id="sgic_agree_radio2" name="sgicagree" />
+                              <label htmlFor="sgic_agree_radio2" className="contentType">동의안함</label>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="sgic_noti">
+                        <p className="sgic_noti_txt">입력하신 개인정보는 서울보증보험㈜의 증권발급을 위해 필요한 정보이며, 다른 용도로 사용되지 않습니다.</p>
+                        <ul className="list_dot">
+                          <li>고객님은 안전거래를 위해 현금 결제 시 소니스토어가 가입한 구매안전서비스 “소비자피해보상보험서비스”를 이용하실 수 있습니다.</li>
+                          <li>물품 대금결제 시 구매자의 피해보호를 위해 ㈜서울보증보험의 보증보험증권이 발급됩니다.</li>
+                          <li>구매안전서비스를 통하여 주문하시고 서울보증보험에서 발행하는 보험계약체결내역서를 반드시 확인하시기 바랍니다.</li>
+                          <li>보상대상 : 미배송, 반송/환불거부, 쇼핑몰부도</li>
+                          <li>보험기간 : 주문일로부터 37일간(37일 보증)</li>
+                          <li>본 전자보증을 신청하시면 물품대금 결제 시에 소비자에게 서울보증보험의 쇼핑몰보증보험 계약 체결서를 인터넷상으로 자동 발급하여, 인터넷 쇼핑몰에서 발행할 수 있는 소비자 피해를 서울보증보험㈜가 보상하는 서비스 입니다.</li>
+                          <li>현금결제 시 순결제금액 기준에 보증보험 전자보증서를 발급받으실 수 있습니다.</li>
+                          <li>본 보증서는 주문 시 신청한 고객에 한해 발급되며, 서비스 제공 기간 동안 유효합니다.</li>
+                          <li>보증금액은 현 주문결제금액 기준으로 발급되며, 주문 변경으로 인한 재발급 및 다른 주문과의 합산발급은 되지 않습니다.</li>
+                          <li>보증보험 전자보증서에 대한 보다 상세한 사항은&nbsp;<a href="http://www.usafe.co.kr/u_esafe.asp" target="_blank" className="under_line"><em className="color">여기</em></a>를 클릭하여 주시기 바랍니다.</li>
+                        </ul>
+                        <div className="common__table__wrap">
+                          <table className="common__table" role="presentation">
+                            <caption>개인정보를 제공받는 자 등의 정보</caption>
+                            <colgroup>
+                              <col width="26%" />
+                              <col width="74%" />
+                            </colgroup>
+                            <tbody>
+                              <tr>
+                                <th scope="row">제공받는 자</th>
+                                <td>㈜서울보증보험, ㈜유세이프</td>
+                              </tr>
+                              <tr>
+                                <th scope="row">제공하는 항목</th>
+                                <td>구매자명,구매자 생년월일,구매자 성별,구매자 연락처 (일반전화 및 핸드폰),구매자 이메일,주문번호,배송지정보,주문금액</td>
+                              </tr>
+                              <tr>
+                                <th scope="row">제공목적</th>
+                                <td>쇼핑몰 보증보험 가입 및 보험가입 제반사항</td>
+                              </tr>
+                              <tr>
+                                <th scope="row">보유 및 이용기간</th>
+                                <td>개별서비스 제공기간</td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
                 <strong className="info_tit3">[소비자
                   피해보상보험
