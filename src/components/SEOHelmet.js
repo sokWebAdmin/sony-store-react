b35dc71fbcdc9dc34e11398740262c2ac02a87a6
Jerome <jerome@geekstudio.co.kr>
2022-04-28 17:57:43 +0900

    feat: SEOHelment Component에 useScript hooks 추가

--- src/components/SEOHelmet.js
+++ src/components/SEOHelmet.js
@@ -1,13 +1,18 @@
 import { Helmet } from 'react-helmet';
 
-// TODO: 삭제예정
-const SEOHelmet = ({ title, children }) => (
-    <>
-        <Helmet>
-            <title>{title}</title>
-            {children}
-        </Helmet>
-    </>
-);
+import useScript from 'hooks/useScript';
+
+const SEOHelmet = ({ title, children }) => {
+    useScript('//image.sony.co.kr/omniture/omni_dev/sonystore_code_2013.js');
+
+    return (
+        <>
+            <Helmet>
+                <title>{title}</title>
+                {children}
+            </Helmet>
+        </>
+    );
+};
 
 export default SEOHelmet;
