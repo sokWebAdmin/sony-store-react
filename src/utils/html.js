066b223cc979e59318244ff8f5772da413561175
Jerome <jerome@geekstudio.co.kr>
2022-05-08 19:38:07 +0900

    fix: class -> className 수정

--- src/utils/html.js
+++ src/utils/html.js
@@ -1,16 +1,16 @@
 const highlightCategory = (label, keyword) => {
     if (label.includes(keyword)) {
         const [prev, next] = label.split(keyword);
-        return `<span class="keyword categoryList">${prev}${keyword}${next}</span>`;
+        return `<span className="keyword categoryList">${prev}${keyword}${next}</span>`;
     }
 
-    return `<span class="categoryList">${label}</span>`;
+    return `<span className="categoryList">${label}</span>`;
 };
 
 const highlightKeyword = (title, keyword) => {
     if (title.includes(keyword)) {
         const [prev, next] = title.split(keyword);
-        return `${prev}<strong class="keyword">${keyword}</strong>${next}`;
+        return `${prev}<strong className="keyword">${keyword}</strong>${next}`;
     }
     return title;
 };
