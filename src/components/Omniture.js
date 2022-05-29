da94c4f19fba420747a3c7fc6b630bd1850023db
jerome <jerome@geekstudio.co.kr>
2022-05-29 18:35:19 +0900

    옴니추어 스크립트 주소 수정

--- src/components/Omniture.js
+++ src/components/Omniture.js
@@ -1,11 +1,7 @@
 import useScript from 'hooks/useScript';
 
 const Omniture = () => {
-    useScript(
-        process.env.NODE_ENV === 'production'
-            ? '//image.sony.co.kr/omniture/real/sonystore_code_2013.js'
-            : '//image.sony.co.kr/omniture/omni_dev/sonystore_code_2013.js',
-    );
+    useScript('//image.sony.co.kr/omniture/omni_dev/sonystore_code_2013.js');
 
     return <></>;
 };
