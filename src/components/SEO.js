dae6cc426f6761436d742e4aacc748f83320b33e
jerome <jerome@geekstudio.co.kr>
2022-05-29 16:08:20 +0900

    test: omniture 스크립트 로딩 위치 변경

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,95 +1,16 @@
-import { Helmet, HelmetProvider } from 'react-helmet-async';
 import PropTypes from 'prop-types';
-
-import useScript from 'hooks/useScript';
+import Omniture from 'components/Omniture';
+import SonyHelmet from 'components/SonyHelmet';
 
 const SEO = ({ data }) => {
     // TODO: 추후 별도 파일로 분리
     const config = { title: 'SonyStore', description: '' };
 
-    useScript('//image.sony.co.kr/omniture/real/sonystore_code_2013.js');
-
     return (
-        <HelmetProvider>
-            <Helmet>
-                {/* 타이틀  */}
-                <title>{data.title || config.title}</title>
-
-                {/* Canonical tag */}
-                {data.canonical && (
-                    <link rel='canonical' href={data.canonical} />
-                )}
-
-                {/* 메타데이터 */}
-                {data.meta?.title && (
-                    <meta
-                        name='title'
-                        content={data.meta.title || config.title}
-                    />
-                )}
-                {data.meta?.description && (
-                    <meta
-                        name='description'
-                        content={data.meta.description || config.description}
-                    />
-                )}
-
-                {/* 오픈그래프 */}
-                <meta property='og:type' content='website' />
-                {data.og?.title && (
-                    <meta property='og:title' content={data.og.title} />
-                )}
-                {data.og?.description && (
-                    <meta
-                        property='og:description'
-                        content={data.og.description}
-                    />
-                )}
-                {data.og?.image && (
-                    <meta property='og:image' content={data.og.image} />
-                )}
-
-                {/* 트위터 */}
-                {data.twitter?.card && (
-                    <meta property='twitter:card' content={data.twitter.card} />
-                )}
-                {data.twitter?.title && (
-                    <meta
-                        property='twitter:title'
-                        content={data.twitter.title}
-                    />
-                )}
-                {data.twitter?.description && (
-                    <meta
-                        property='twitter:description'
-                        content={data.twitter.description}
-                    />
-                )}
-                {data.twitter?.image && (
-                    <meta
-                        property='twitter:image'
-                        content={data.twitter.image}
-                    />
-                )}
-
-                {/* itemprop */}
-                {data.itemprop?.name && (
-                    <meta itemprop='name' content={data.itemprop.name || ''} />
-                )}
-                {data.itemprop?.description && (
-                    <meta
-                        itemprop='description'
-                        content={data.itemprop.description || ''}
-                    />
-                )}
-                {data.itemprop?.image && (
-                    <meta
-                        itemprop='image'
-                        content={data.itemprop.image || ''}
-                    />
-                )}
-            </Helmet>
-        </HelmetProvider>
+        <>
+            <SonyHelmet data={data} />
+            {data?.title && <Omniture />}
+        </>
     );
 };
 
