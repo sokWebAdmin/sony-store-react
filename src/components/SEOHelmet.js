0c4b283e6f70a18d32466fbfd0106046240425fe
sokWebAdmin <86636809+sokWebAdmin@users.noreply.github.com>
2022-05-29 22:00:07 +0900

    [임시] 옴니츄어 테스트
    [임시] 옴니츄어 테스트

--- src/components/SEOHelmet.js
+++ src/components/SEOHelmet.js
@@ -3,8 +3,10 @@ import { Helmet } from 'react-helmet';
 import useScript from 'hooks/useScript';
 
 const SEOHelmet = ({ title, children }) => {
-    useScript('//image.sony.co.kr/omniture/real/sonystore_code_2013.js');
-
+    //useScript('//image.sony.co.kr/omniture/real/sonystore_code_2013.js');
+    
+    useScript('//image.sony.co.kr/omniture/omni_dev/sonystore_code_2013.js');
+    
     return (
         <>
             <Helmet>
